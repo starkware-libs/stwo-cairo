63164c056ece4647601cf9d2b2e5a479d726e6aa
Shahar Samocha <shahars@starkware.co>
2024-08-01 20:30:00 +0300

    rangecheck128 interaction trace

--- stwo_cairo_prover/src/components/range_check_builtin/simd_trace.rs
+++ stwo_cairo_prover/src/components/range_check_builtin/simd_trace.rs
@@ -1,10 +1,13 @@
 use itertools::{chain, Itertools};
 use num_traits::Zero;
+use stwo_prover::constraint_framework::logup::{LogupTraceGenerator, LookupElements};
 use stwo_prover::core::backend::simd::column::BaseColumn;
 use stwo_prover::core::backend::simd::m31::{PackedM31, LOG_N_LANES, N_LANES};
+use stwo_prover::core::backend::simd::qm31::PackedQM31;
 use stwo_prover::core::backend::simd::SimdBackend;
 use stwo_prover::core::backend::{Col, Column};
 use stwo_prover::core::fields::m31::{BaseField, M31};
+use stwo_prover::core::fields::qm31::SecureField;
 use stwo_prover::core::poly::circle::{CanonicCoset, CircleEvaluation};
 use stwo_prover::core::poly::BitReversedOrder;
 use stwo_prover::core::ColumnVec;
@@ -74,6 +77,32 @@ pub fn generate_trace(
     )
 }
 
+pub fn gen_interaction_trace(
+    log_size: u32,
+    lookup_data: RangeCheck128BuiltinLookupData,
+    memory_lookup_elements: &LookupElements,
+    rcunit2_lookup_elements: &LookupElements,
+) -> (
+    ColumnVec<CircleEvaluation<SimdBackend, BaseField, BitReversedOrder>>,
+    SecureField,
+) {
+    let mut logup_gen = LogupTraceGenerator::new(log_size);
+    let mut col_gen = logup_gen.new_col();
+    for vec_row in 0..(1 << (log_size - LOG_N_LANES)) {
+        let p_mem: PackedQM31 = memory_lookup_elements.combine(
+            &lookup_data
+                .memory_lookups
+                .each_ref()
+                .map(|l| l.data[vec_row]),
+        );
+        let p_rc2: PackedQM31 =
+            rcunit2_lookup_elements.combine(&[lookup_data.rcunit2_lookups[0].data[vec_row]]);
+        col_gen.write_frac(vec_row, p_mem + p_rc2, p_mem * p_rc2);
+    }
+    col_gen.finalize_col();
+    logup_gen.finalize()
+}
+
 // TODO: replace this code with prover_types when available.
 fn split_u128(mut x: [u128; N_LANES]) -> [PackedM31; N_VALUES_FELTS] {
     let mut res = [PackedM31::zero(); N_VALUES_FELTS];
@@ -91,11 +120,16 @@ mod tests {
     use std::array;
 
     use rand::Rng;
+    use stwo_prover::constraint_framework::constant_columns::gen_is_first;
+    use stwo_prover::constraint_framework::FrameworkComponent;
+    use stwo_prover::core::fields::qm31::SecureField;
+    use stwo_prover::core::pcs::TreeVec;
+
+    use super::*;
+    use crate::components::range_check_builtin::component::RangeCheck128BuiltinComponent;
 
     #[test]
     fn test_generate_trace() {
-        use super::*;
-
         let log_size = 8;
         let address_initial_offset = 1000;
         let mut rng = rand::thread_rng();
@@ -126,7 +160,6 @@ mod tests {
                 input >>= N_BITS_PER_FELT;
             }
         }
-
         // Assert rcunit2_lookups are in range [0, 4).
         for rc2_col in lookup_data.rcunit2_lookups.iter() {
             for row in rc2_col.data.iter() {
@@ -147,5 +180,63 @@ mod tests {
                 })
             );
         }
+    }
+
+    #[test]
+    fn test_interaction_trace() {
+        let log_size = 8;
+        let address_initial_offset = 10032;
+        let inputs = (0..32)
+            .map(|i| RangeCheckInput {
+                address: PackedM31::from_array(array::from_fn(|j| {
+                    M31::from_u32_unchecked(i * N_LANES as u32 + j as u32 + address_initial_offset)
+                })),
+                values: array::from_fn(|_| 85237070383327051513330625736333328897_u128),
+            })
+            .collect_vec();
+
+        let (trace, lookup_data) =
+            generate_trace(log_size, M31::from(address_initial_offset), &inputs);
+
+        let memory_lookup_elements = LookupElements {
+            z: SecureField::from_m31_array(array::from_fn(|i| M31::from_u32_unchecked(i as u32))),
+            alpha: SecureField::from_m31_array(array::from_fn(|i| {
+                M31::from_u32_unchecked(i as u32)
+            })),
+        };
+        let rc_lookup_elements = LookupElements {
+            z: SecureField::from_m31_array(array::from_fn(|i| {
+                M31::from_u32_unchecked(3 * i as u32)
+            })),
+            alpha: SecureField::from_m31_array(array::from_fn(|i| {
+                M31::from_u32_unchecked(4 * i as u32)
+            })),
+        };
+
+        let (interaction_trace, claimed_sum) = gen_interaction_trace(
+            log_size,
+            lookup_data,
+            &memory_lookup_elements,
+            &rc_lookup_elements,
+        );
+
+        let trace = TreeVec::new(vec![trace, interaction_trace, vec![gen_is_first(log_size)]]);
+        let trace_polys = trace.map_cols(|c| c.interpolate());
+
+        let component = RangeCheck128BuiltinComponent {
+            log_size,
+            initial_memory_address: M31::from(address_initial_offset),
+            memory_lookup_elements,
+            rcunit2_lookup_elements: rc_lookup_elements,
+            claimed_sum,
+        };
+
+        stwo_prover::constraint_framework::assert_constraints(
+            &trace_polys,
+            CanonicCoset::new(log_size),
+            |eval| {
+                component.evaluate(eval);
+            },
+        )
     }
 }
