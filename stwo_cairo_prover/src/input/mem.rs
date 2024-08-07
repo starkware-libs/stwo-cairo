db71d1d51fd11e8354ce486893131ae5f25367f7
Shahar Papini <spapini@starkware.co>
2024-08-07 14:59:53 +0300

    Cairo Memory

---
+++ stwo_cairo_prover/src/input/mem.rs
@@ -0,0 +1,158 @@
+use super::vm_import::MemEntry;
+
+pub const SMALL_VALUE_SHIFT: i32 = 1 << 16;
+
+// TODO(spapini): Add U26 for addresses and U128 for range checks.
+// TODO(spapini): Use some struct for Felt252 (that is still memory efficient).
+#[derive(Default)]
+pub struct Memory {
+    pub address_to_id: Vec<EncodedMemoryValueId>,
+    pub u32_values: Vec<u32>,
+    pub u64_values: Vec<u64>,
+    pub f252_values: Vec<[u32; 8]>,
+}
+impl Memory {
+    pub fn get(&self, addr: u32) -> MemoryValue {
+        match self.address_to_id[addr as usize].decode() {
+            MemoryValueId::I16(id) => MemoryValue::I16(id),
+            MemoryValueId::U32(id) => MemoryValue::U32(self.u32_values[id]),
+            MemoryValueId::U64(id) => MemoryValue::U64(self.u64_values[id]),
+            MemoryValueId::F252(id) => MemoryValue::F252(self.f252_values[id]),
+        }
+    }
+    pub fn set(&mut self, addr: u64, value: MemoryValue) {
+        if addr as usize >= self.address_to_id.len() {
+            self.address_to_id
+                .resize(addr as usize + 1, EncodedMemoryValueId(0));
+        }
+        self.address_to_id[addr as usize] = EncodedMemoryValueId::encode(match value {
+            MemoryValue::I16(id) => MemoryValueId::I16(id),
+            MemoryValue::U32(id) => {
+                let res = self.u32_values.len();
+                self.u32_values.push(id);
+                MemoryValueId::U32(res)
+            }
+            MemoryValue::U64(id) => {
+                let res = self.u64_values.len();
+                self.u64_values.push(id);
+                MemoryValueId::U64(res)
+            }
+            MemoryValue::F252(id) => {
+                let res = self.f252_values.len();
+                self.f252_values.push(id);
+                MemoryValueId::F252(res)
+            }
+        })
+    }
+}
+
+impl FromIterator<MemEntry> for Memory {
+    fn from_iter<I: IntoIterator<Item = MemEntry>>(iter: I) -> Memory {
+        let mem_entries = iter.into_iter();
+        let mut memory = Memory::default();
+        for entry in mem_entries {
+            memory.set(entry.addr, MemoryValue::from(entry.val));
+        }
+        memory
+    }
+}
+
+#[derive(Copy, Clone, Default)]
+pub struct EncodedMemoryValueId(u32);
+impl EncodedMemoryValueId {
+    pub fn encode(value: MemoryValueId) -> EncodedMemoryValueId {
+        match value {
+            MemoryValueId::I16(id) => EncodedMemoryValueId((id + SMALL_VALUE_SHIFT) as u32),
+            MemoryValueId::U32(id) => EncodedMemoryValueId(id as u32 | 0x4000_0000),
+            MemoryValueId::U64(id) => EncodedMemoryValueId(id as u32 | 0x8000_0000),
+            MemoryValueId::F252(id) => EncodedMemoryValueId(id as u32 | 0xc000_0000),
+        }
+    }
+    pub fn decode(&self) -> MemoryValueId {
+        let tag = self.0 >> 30;
+        let val = self.0 & 0x3FFF_FFFF;
+        match tag {
+            0 => MemoryValueId::I16(val as i32 - SMALL_VALUE_SHIFT),
+            1 => MemoryValueId::U32(val as usize),
+            2 => MemoryValueId::U64(val as usize),
+            3 => MemoryValueId::F252(val as usize),
+            _ => panic!("Invalid tag"),
+        }
+    }
+}
+
+pub enum MemoryValueId {
+    I16(i32),
+    U32(usize),
+    U64(usize),
+    F252(usize),
+}
+
+#[derive(Copy, Clone, PartialEq, Eq, Debug)]
+pub enum MemoryValue {
+    I16(i32),
+    U32(u32),
+    U64(u64),
+    F252([u32; 8]),
+}
+impl MemoryValue {
+    pub fn as_u32(&self) -> u32 {
+        match self {
+            MemoryValue::I16(x) => *x as u32,
+            MemoryValue::U32(x) => *x,
+            MemoryValue::U64(_) => panic!("Cannot convert u64 to u32"),
+            MemoryValue::F252(_) => panic!("Cannot convert F252 to u32"),
+        }
+    }
+    pub fn as_u64(&self) -> u64 {
+        match self {
+            MemoryValue::I16(x) => (*x).try_into().unwrap(),
+            MemoryValue::U32(x) => *x as u64,
+            MemoryValue::U64(x) => *x,
+            MemoryValue::F252(_) => panic!("Cannot convert F252 to u64"),
+        }
+    }
+}
+impl From<[u32; 8]> for MemoryValue {
+    fn from(value: [u32; 8]) -> MemoryValue {
+        if !value[2..].iter().all(|&x| x == 0) {
+            return MemoryValue::F252(value);
+        }
+        if value[1] != 0 {
+            return MemoryValue::U64((value[0] as u64) | ((value[1] as u64) << 32));
+        }
+        if value[0] >= SMALL_VALUE_SHIFT as u32 {
+            return MemoryValue::U32(value[0]);
+        }
+        // TODO: Handle small negatives.
+        MemoryValue::I16(value[0] as i32)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+
+    #[test]
+    fn test_memory() {
+        let entries = [
+            MemEntry {
+                addr: 0,
+                val: [1; 8],
+            },
+            MemEntry {
+                addr: 1,
+                val: [6, 0, 0, 0, 0, 0, 0, 0],
+            },
+            MemEntry {
+                addr: 2,
+                val: [1, 2, 0, 0, 0, 0, 0, 0],
+            },
+        ];
+        let memory = Memory::from_iter(entries.iter().cloned());
+        assert_eq!(memory.get(0), MemoryValue::F252([1; 8]));
+        assert_eq!(memory.get(1), MemoryValue::I16(6));
+        assert_eq!(memory.get(2), MemoryValue::U64((2 << 32) | 1));
+    }
+}
