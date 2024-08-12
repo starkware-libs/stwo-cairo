0cca086bdd5d7d9bd6b304d1d4fb00e3f7256b52
Shahar Papini <spapini@starkware.co>
2024-08-12 17:05:44 +0300

    Dynamic memory small range

--- stwo_cairo_prover/src/input/mem.rs
+++ stwo_cairo_prover/src/input/mem.rs
@@ -1,10 +1,5 @@
 use super::vm_import::MemEntry;
 
-// TODO: Make these dynamic.
-// Assumption: These will always be less than 32 bits.
-pub const SMALL_MIN_NEG: u32 = (1 << 20) - 1;
-pub const SMALL_MAX: u32 = (1 << 20) - 1;
-
 /// Prime 2^251 + 17 * 2^192 + 1 in little endian.
 const P_MIN_1: [u32; 8] = [
     0x0000_0000,
@@ -27,15 +22,48 @@ const P_MIN_2: [u32; 8] = [
     0x0800_0000,
 ];
 
+// Note: this should be smaller than 2^29.
+const SMALL_VALUE_SHIFT: u32 = 1 << 26;
+pub struct MemConfig {
+    small_min_neg: u32,
+    small_max: u32,
+}
+impl MemConfig {
+    pub fn new(small_min_neg: u32, small_max: u32) -> MemConfig {
+        assert!(small_min_neg <= SMALL_VALUE_SHIFT);
+        assert!(small_max <= SMALL_VALUE_SHIFT);
+        MemConfig {
+            small_min_neg,
+            small_max,
+        }
+    }
+}
+impl Default for MemConfig {
+    fn default() -> Self {
+        MemConfig {
+            small_min_neg: (1 << 20) - 1,
+            small_max: (1 << 20) - 1,
+        }
+    }
+}
+
 // TODO(spapini): Add U26 for addresses and U128 for range checks.
 // TODO(spapini): Use some struct for Felt252 (that is still memory efficient).
-#[derive(Default)]
 pub struct Memory {
+    pub config: MemConfig,
     pub address_to_id: Vec<EncodedMemoryValueId>,
     pub u64_values: Vec<u64>,
     pub f252_values: Vec<[u32; 8]>,
 }
 impl Memory {
+    fn new(config: MemConfig) -> Memory {
+        Memory {
+            config,
+            address_to_id: Vec::new(),
+            u64_values: Vec::new(),
+            f252_values: Vec::new(),
+        }
+    }
     pub fn get(&self, addr: u32) -> MemoryValue {
         match self.address_to_id[addr as usize].decode() {
             MemoryValueId::Small(id) => MemoryValue::Small(id),
@@ -62,16 +90,42 @@ impl Memory {
             }
         })
     }
-}
-
-impl FromIterator<MemEntry> for Memory {
-    fn from_iter<I: IntoIterator<Item = MemEntry>>(iter: I) -> Memory {
+    pub fn from_iter<I: IntoIterator<Item = MemEntry>>(config: MemConfig, iter: I) -> Memory {
         let mem_entries = iter.into_iter();
-        let mut memory = Memory::default();
+        let mut memory = Memory::new(config);
         for entry in mem_entries {
-            memory.set(entry.addr, MemoryValue::from(entry.val));
+            let value = memory.value_from_felt252(entry.val);
+            memory.set(entry.addr, value);
         }
         memory
+    }
+    // TODO(spapini): Optimize. This should be SIMD.
+    fn value_from_felt252(&self, value: [u32; 8]) -> MemoryValue {
+        if value[7] == 0 {
+            // Positive case.
+            if value[2..7] != [0; 5] {
+                // Over 64bit.
+                return MemoryValue::F252(value);
+            }
+            if value[1] != 0 || value[0] > self.config.small_max {
+                // Not small.
+                return MemoryValue::U64((value[0] as u64) | ((value[1] as u64) << 32));
+            }
+            MemoryValue::Small(value[0] as i32)
+        } else {
+            // Negative case.
+            if value == P_MIN_1 {
+                return MemoryValue::Small(-1);
+            }
+            if value[1..7] != P_MIN_2[1..7] {
+                return MemoryValue::F252(value);
+            }
+            let num = 0xFFFF_FFFF - value[0];
+            if num < self.config.small_min_neg - 2 {
+                return MemoryValue::Small(-(num as i32 + 2));
+            }
+            MemoryValue::F252(value)
+        }
     }
 }
 
@@ -80,7 +134,9 @@ pub struct EncodedMemoryValueId(u32);
 impl EncodedMemoryValueId {
     pub fn encode(value: MemoryValueId) -> EncodedMemoryValueId {
         match value {
-            MemoryValueId::Small(id) => EncodedMemoryValueId((id + SMALL_MIN_NEG as i32) as u32),
+            MemoryValueId::Small(id) => {
+                EncodedMemoryValueId((id + SMALL_VALUE_SHIFT as i32) as u32)
+            }
             MemoryValueId::U64(id) => EncodedMemoryValueId(id as u32 | 0x4000_0000),
             MemoryValueId::F252(id) => EncodedMemoryValueId(id as u32 | 0x8000_0000),
         }
@@ -89,7 +145,7 @@ impl EncodedMemoryValueId {
         let tag = self.0 >> 30;
         let val = self.0 & 0x3FFF_FFFF;
         match tag {
-            0 => MemoryValueId::Small(val as i32 - SMALL_MIN_NEG as i32),
+            0 => MemoryValueId::Small(val as i32 - SMALL_VALUE_SHIFT as i32),
             1 => MemoryValueId::U64(val as usize),
             2 => MemoryValueId::F252(val as usize),
             _ => panic!("Invalid tag"),
@@ -122,36 +178,6 @@ impl MemoryValue {
             MemoryValue::Small(x) => (*x).try_into().unwrap(),
             MemoryValue::U64(x) => *x,
             MemoryValue::F252(_) => panic!("Cannot convert F252 to u64"),
-        }
-    }
-}
-impl From<[u32; 8]> for MemoryValue {
-    // TODO(spapini): Optimize. This should be SIMD.
-    fn from(value: [u32; 8]) -> MemoryValue {
-        if value[7] == 0 {
-            // Positive case.
-            if value[2..7] != [0; 5] {
-                // Over 64bit.
-                return MemoryValue::F252(value);
-            }
-            if value[1] != 0 || value[0] > SMALL_MAX {
-                // Not small.
-                return MemoryValue::U64((value[0] as u64) | ((value[1] as u64) << 32));
-            }
-            MemoryValue::Small(value[0] as i32)
-        } else {
-            // Negative case.
-            if value == P_MIN_1 {
-                return MemoryValue::Small(-1);
-            }
-            if value[1..7] != P_MIN_2[1..7] {
-                return MemoryValue::F252(value);
-            }
-            let num = 0xFFFF_FFFF - value[0];
-            if num < SMALL_MIN_NEG - 2 {
-                return MemoryValue::Small(-(num as i32 + 2));
-            }
-            MemoryValue::F252(value)
         }
     }
 }
@@ -189,7 +215,7 @@ mod tests {
                 val: P_MIN_2,
             },
         ];
-        let memory = Memory::from_iter(entries.iter().cloned());
+        let memory = Memory::from_iter(MemConfig::default(), entries.iter().cloned());
         assert_eq!(memory.get(0), MemoryValue::F252([1; 8]));
         assert_eq!(memory.get(1), MemoryValue::Small(6));
         assert_eq!(memory.get(2), MemoryValue::U64((2 << 32) | 1));
