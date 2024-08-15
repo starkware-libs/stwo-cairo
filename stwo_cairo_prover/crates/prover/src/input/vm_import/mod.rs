4a3f59b108269c97ee74bab61b8c1f00b1bd5fbe
Shahar Papini <spapini@starkware.co>
2024-08-15 13:13:27 +0300

    Read proof jsons

--- stwo_cairo_prover/crates/prover/src/input/vm_import/mod.rs
+++ stwo_cairo_prover/crates/prover/src/input/vm_import/mod.rs
@@ -1,18 +1,36 @@
+mod json;
+
 use std::io::Read;
 use std::path::Path;
 
 use bytemuck::{bytes_of_mut, Pod, Zeroable};
+use json::{PrivateInput, PublicInput};
 
 use super::instructions::Instructions;
 use super::mem::{MemConfig, Memory};
 use super::CairoInput;
 
-pub fn import_from_vm_output(trace_path: &Path, mem_path: &Path) -> CairoInput {
-    let mut mem_file = std::io::BufReader::new(std::fs::File::open(mem_path).unwrap());
-    // TODO(spapini): dynamically understand the memory configuration.
-    let mem_config = MemConfig::default();
-    let mem = Memory::from_iter(mem_config, MemEntryIter(&mut mem_file));
+pub fn import_from_vm_output(pub_json: &Path, priv_json: &Path) -> CairoInput {
+    let pub_data: PublicInput =
+        sonic_rs::from_str(&std::fs::read_to_string(pub_json).unwrap()).unwrap();
+    let priv_data: PrivateInput =
+        sonic_rs::from_str(&std::fs::read_to_string(priv_json).unwrap()).unwrap();
+
+    let end_addr = pub_data
+        .memory_segments
+        .values()
+        .map(|v| v.stop_ptr)
+        .max()
+        .unwrap();
+    assert!(end_addr < (1 << 32));
+    let mem_config = MemConfig::new((1 << 20) - 1, end_addr as u32);
+
+    let mem_path = priv_json.parent().unwrap().join(&priv_data.memory_path);
+    let trace_path = priv_json.parent().unwrap().join(&priv_data.trace_path);
+
     let mut trace_file = std::io::BufReader::new(std::fs::File::open(trace_path).unwrap());
+    let mut mem_file = std::io::BufReader::new(std::fs::File::open(mem_path).unwrap());
+    let mem = Memory::from_iter(mem_config, MemEntryIter(&mut mem_file));
     let instructions = Instructions::from_iter(TraceIter(&mut trace_file), &mem);
 
     CairoInput { instructions, mem }
@@ -74,25 +92,26 @@ mod tests {
     #[test]
     fn test_read_from_files() {
         let mut d = PathBuf::from(env!("CARGO_MANIFEST_DIR"));
-        d.push("test_data");
+        d.push("test_data/sample0");
         let input =
-            import_from_vm_output(d.join("trace_file").as_path(), d.join("mem_file").as_path());
+            import_from_vm_output(d.join("pub.json").as_path(), d.join("priv.json").as_path());
         assert_eq!(
             input.instructions.usage(),
             InstructionUsage {
-                ret: 99,
-                add_ap: 208,
-                jmp_rel_imm: [189, 0],
+                ret: 49472,
+                add_ap: 36895,
+                jmp_rel_imm: [31873866, 0],
                 jmp_abs: [0, 0, 0, 0],
-                call_rel_imm: 105,
-                call_abs: [0, 0],
-                jnz_imm: [27, 5, 180, 17, 0, 0, 12, 0],
-                mov_mem: 302,
-                deref: 6671,
-                push_imm: 53,
-                generic: 2132
+                call_rel_imm: 49439,
+                call_abs: [0, 33],
+                jnz_imm: [20957, 5100, 30113, 11235, 6075, 0, 20947, 0],
+                mov_mem: 233432,
+                deref: 811061,
+                push_imm: 43184,
+                generic: 362623
             }
         );
+        println!("Usage: {:#?}", input.instructions.usage());
     }
 
     #[ignore]
