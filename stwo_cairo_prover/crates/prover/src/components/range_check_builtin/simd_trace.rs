83f313bd3ba135186e9a3ee45e232f2716ea964d
Alon Haramati <alonh@starkware.co>
2024-08-22 16:50:28 +0300

    Adjust range check builtin.

--- stwo_cairo_prover/crates/prover/src/components/range_check_builtin/simd_trace.rs
+++ stwo_cairo_prover/crates/prover/src/components/range_check_builtin/simd_trace.rs
@@ -1,79 +1,136 @@
-use std::simd::u32x16;
-
 use itertools::{chain, Itertools};
-use num_traits::Zero;
 use stwo_prover::constraint_framework::logup::LogupTraceGenerator;
-use stwo_prover::core::backend::simd::column::BaseColumn;
-use stwo_prover::core::backend::simd::m31::{PackedM31, LOG_N_LANES};
+use stwo_prover::core::backend::simd::m31::{PackedM31, LOG_N_LANES, N_LANES};
 use stwo_prover::core::backend::simd::qm31::PackedQM31;
 use stwo_prover::core::backend::simd::SimdBackend;
 use stwo_prover::core::backend::{Col, Column};
 use stwo_prover::core::fields::m31::{BaseField, M31};
 use stwo_prover::core::fields::qm31::SecureField;
+use stwo_prover::core::pcs::TreeBuilder;
 use stwo_prover::core::poly::circle::{CanonicCoset, CircleEvaluation};
 use stwo_prover::core::poly::BitReversedOrder;
+use stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleHasher;
 use stwo_prover::core::ColumnVec;
 
-use super::component::{LAST_VALUE_OFFSET, N_VALUES_FELTS};
+use super::component::{RangeCheckBuiltinClaim, RangeCheckBuiltinInteractionClaim, N_VALUES_FELTS};
+use crate::components::memory::prover::MemoryClaimProver;
 use crate::components::memory::{MemoryLookupElements, N_ADDRESS_FELTS};
 use crate::components::range_check_unit::RangeElements;
-use crate::felt::split_u128_simd;
+use crate::components::MIN_SIMD_TRACE_LENGTH;
+use crate::input::SegmentAddrs;
 
-// Memory addresses and the corresponding values, for the RangeCheck128Builtin segment.
-pub struct RangeCheckInput {
-    pub address: PackedM31,
-    pub values: [u32x16; 4],
+// Memory addresses for the RangeCheckBuiltin segment.
+pub type RangeCheckBuiltinInput = PackedM31;
+
+pub struct RangeCheckClaimProver {
+    pub memory_segment: SegmentAddrs,
 }
 
-impl Default for RangeCheckInput {
-    fn default() -> Self {
+impl RangeCheckClaimProver {
+    pub fn new(input: SegmentAddrs) -> Self {
         Self {
-            address: PackedM31::zero(),
-            values: [u32x16::splat(0); 4],
+            memory_segment: input,
+        }
+    }
+
+    pub fn write_trace(
+        &self,
+        tree_builder: &mut TreeBuilder<'_, '_, SimdBackend, Blake2sMerkleHasher>,
+        memory_trace_generator: &mut MemoryClaimProver,
+    ) -> (RangeCheckBuiltinClaim, RangeCheckBuiltinInteractionProver) {
+        let mut addresses = self.memory_segment.addresses();
+        // TODO(spapini): Split to multiple components.
+        let size = addresses
+            .len()
+            .next_power_of_two()
+            .max(MIN_SIMD_TRACE_LENGTH);
+        addresses.resize(size, 0);
+
+        let inputs = addresses
+            .into_iter()
+            .array_chunks::<N_LANES>()
+            .map(|chunk| {
+                PackedM31::from_array(std::array::from_fn(|i| M31::from_u32_unchecked(chunk[i])))
+            })
+            .collect_vec();
+        let (trace, interaction_prover) = write_trace_simd(&inputs, memory_trace_generator);
+        interaction_prover
+            .memory_addresses
+            .iter()
+            .for_each(|v| memory_trace_generator.add_inputs_simd(v));
+        tree_builder.extend_evals(trace);
+        let claim = RangeCheckBuiltinClaim {
+            memory_segment: self.memory_segment.clone(),
+        };
+        (claim, interaction_prover)
+    }
+}
+
+pub struct RangeCheckBuiltinInteractionProver {
+    pub memory_addresses: Vec<PackedM31>,
+    pub memory_values: Vec<[PackedM31; N_VALUES_FELTS]>,
+}
+
+impl RangeCheckBuiltinInteractionProver {
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            memory_addresses: Vec::with_capacity(capacity),
+            memory_values: Vec::with_capacity(capacity),
+        }
+    }
+
+    pub fn write_interaction_trace(
+        &self,
+        tree_builder: &mut TreeBuilder<'_, '_, SimdBackend, Blake2sMerkleHasher>,
+        memory_lookup_elements: &MemoryLookupElements,
+        range2_lookup_elements: &RangeElements,
+    ) -> RangeCheckBuiltinInteractionClaim {
+        let log_size = self.memory_addresses.len().ilog2() + LOG_N_LANES;
+        let (trace, claimed_sum) = gen_interaction_trace(
+            self,
+            log_size,
+            memory_lookup_elements,
+            range2_lookup_elements,
+        );
+        tree_builder.extend_evals(trace);
+
+        RangeCheckBuiltinInteractionClaim {
+            log_size,
+            claimed_sum,
         }
     }
 }
 
-pub struct RangeCheck128BuiltinLookupData {
-    pub memory_lookups: [BaseColumn; N_ADDRESS_FELTS + N_VALUES_FELTS],
-}
-impl RangeCheck128BuiltinLookupData {
-    fn new(log_size: u32) -> Self {
-        Self {
-            memory_lookups: std::array::from_fn(|_| unsafe {
-                BaseColumn::uninitialized(1 << log_size)
-            }),
-        }
-    }
-}
-
-pub fn generate_trace(
-    log_size: u32,
-    address_initial_offset: M31,
-    inputs: &[RangeCheckInput],
+pub fn write_trace_simd(
+    inputs: &[RangeCheckBuiltinInput],
+    memory_trace_generator: &MemoryClaimProver,
 ) -> (
     ColumnVec<CircleEvaluation<SimdBackend, BaseField, BitReversedOrder>>,
-    RangeCheck128BuiltinLookupData,
+    RangeCheckBuiltinInteractionProver,
 ) {
-    let mut lookup_data = RangeCheck128BuiltinLookupData::new(log_size);
+    let log_size = inputs.len().ilog2() + LOG_N_LANES;
+    let mut interaction_prover = RangeCheckBuiltinInteractionProver::with_capacity(inputs.len());
     let mut trace = (0..N_ADDRESS_FELTS + N_VALUES_FELTS)
         .map(|_| unsafe { Col::<SimdBackend, M31>::uninitialized(1 << log_size) })
         .collect_vec();
 
+    let address_initial_offset = PackedM31::broadcast(BaseField::from(inputs[0].into_simd()[0]));
     #[allow(clippy::needless_range_loop)]
     for vec_row in 0..(1 << (log_size - LOG_N_LANES)) {
-        let row_input = inputs.get(vec_row).unwrap();
+        let row_input = *inputs.get(vec_row).unwrap();
         // TODO: remove address from the trace.
-        let address = row_input.address - PackedM31::broadcast(address_initial_offset);
-        let split_values = split_u128_simd(row_input.values);
+        let split_values: [PackedM31; N_VALUES_FELTS] = memory_trace_generator
+            .deduce_output(row_input)[..N_VALUES_FELTS]
+            .try_into()
+            .unwrap();
+        let address = row_input - address_initial_offset;
         trace[0].data[vec_row] = address;
         for (i, v) in split_values.iter().enumerate() {
             trace[i + 1].data[vec_row] = *v;
         }
 
-        chain![vec![row_input.address], split_values]
-            .enumerate()
-            .for_each(|(i, val)| lookup_data.memory_lookups[i].data[vec_row] = val);
+        interaction_prover.memory_addresses.push(row_input);
+        interaction_prover.memory_values.push(split_values);
     }
 
     let domain = CanonicCoset::new(log_size).circle_domain();
@@ -82,13 +139,13 @@ pub fn generate_trace(
             .into_iter()
             .map(|eval| CircleEvaluation::<SimdBackend, _, BitReversedOrder>::new(domain, eval))
             .collect_vec(),
-        lookup_data,
+        interaction_prover,
     )
 }
 
 pub fn gen_interaction_trace(
+    interaction_prover: &RangeCheckBuiltinInteractionProver,
     log_size: u32,
-    lookup_data: RangeCheck128BuiltinLookupData,
     memory_lookup_elements: &MemoryLookupElements,
     range2_lookup_elements: &RangeElements,
 ) -> (
@@ -99,16 +156,18 @@ pub fn gen_interaction_trace(
     let mut col_gen = logup_gen.new_col();
     for vec_row in 0..(1 << (log_size - LOG_N_LANES)) {
         let p_mem: PackedQM31 = memory_lookup_elements.combine(
-            &lookup_data
-                .memory_lookups
-                .each_ref()
-                .map(|l| l.data[vec_row]),
+            &chain!(
+                [interaction_prover.memory_addresses[vec_row]],
+                interaction_prover.memory_values[vec_row]
+            )
+            .collect_vec(),
         );
         let p_rc2: PackedQM31 = range2_lookup_elements
-            .combine(&[lookup_data.memory_lookups[LAST_VALUE_OFFSET].data[vec_row]]);
+            .combine(&[interaction_prover.memory_values[vec_row][N_VALUES_FELTS - 1]]);
         col_gen.write_frac(vec_row, p_mem + p_rc2, p_mem * p_rc2);
     }
     col_gen.finalize_col();
+
     logup_gen.finalize()
 }
 
@@ -118,7 +177,6 @@ mod tests {
     use std::simd::Simd;
 
     use rand::Rng;
-    use stwo_prover::constraint_framework::constant_columns::gen_is_first;
     use stwo_prover::constraint_framework::FrameworkComponent;
     use stwo_prover::core::backend::simd::m31::N_LANES;
     use stwo_prover::core::channel::{Blake2sChannel, Channel};
@@ -128,8 +186,9 @@ mod tests {
 
     use super::*;
     use crate::components::memory::{MemoryLookupElements, N_BITS_PER_FELT};
-    use crate::components::range_check_builtin::component::RangeCheck128BuiltinComponent;
+    use crate::components::range_check_builtin::component::RangeCheckBuiltinComponent;
     use crate::felt::split_f252;
+    use crate::prover_types::PackedUInt32;
 
     #[test]
     fn test_generate_trace() {
@@ -137,18 +196,30 @@ mod tests {
 
         let mut rng = rand::thread_rng();
         let log_size = 8;
-        let address_initial_offset = 1000;
         let inputs = (0..1 << (log_size - LOG_N_LANES))
-            .map(|i| RangeCheckInput {
-                address: PackedM31::from_array(array::from_fn(|j| {
-                    M31::from_u32_unchecked(i * N_LANES as u32 + j as u32 + address_initial_offset)
-                })),
-                values: array::from_fn(|_| Simd::from_array(rng.gen())),
+            .map(|i| {
+                PackedM31::from_array(array::from_fn(|j| {
+                    M31::from_u32_unchecked(i * N_LANES as u32 + j as u32)
+                }))
             })
             .collect_vec();
 
-        let (trace, lookup_data) =
-            generate_trace(log_size, M31::from(address_initial_offset), &inputs);
+        let values = (0..1 << (log_size - LOG_N_LANES))
+            .map(|_| {
+                array::from_fn(|i| {
+                    if i < 4 {
+                        Simd::from_array(rng.gen())
+                    } else {
+                        Simd::splat(0)
+                    }
+                })
+            })
+            .collect_vec();
+        let memory_trace_generator = MemoryClaimProver {
+            values: values.clone(),
+            multiplicities: vec![PackedUInt32::broadcast(0); 1 << (log_size - LOG_N_LANES)],
+        };
+        let (trace, interaction_prover) = write_trace_simd(&inputs, &memory_trace_generator);
 
         assert_eq!(trace.len(), N_ADDRESS_FELTS + N_VALUES_FELTS);
         assert_eq!(
@@ -159,11 +230,15 @@ mod tests {
         // Assert that the trace values are correct.
         #[allow(clippy::needless_range_loop)]
         for row_offset in 0..1 << (log_size - LOG_N_LANES) {
-            let input = inputs[row_offset].values;
+            let input = values[row_offset];
 
             let mut inputs_u128 = [0_u128; 16];
             for (index, simd) in input.iter().enumerate() {
                 for (i, val) in simd.to_array().into_iter().enumerate() {
+                    if index >= 4 {
+                        assert_eq!(val, 0);
+                        continue;
+                    }
                     let val_u128 = val as u128;
                     inputs_u128[i] += val_u128 << (32 * index);
                 }
@@ -180,21 +255,19 @@ mod tests {
         }
 
         // Assert that the high values are in range [0, 4).
-        let high_values_col = &lookup_data.memory_lookups[LAST_VALUE_OFFSET];
-        for packed_row in high_values_col.data.iter() {
+        for output in &interaction_prover.memory_values {
+            let packed_row = output[N_VALUES_FELTS - 1];
             assert!(packed_row.to_array().iter().all(|&x| x.0 < 4));
         }
-        // Assert memory addresses lookup are sequential, offseted by `address_initial_offset`.
+        // Assert memory addresses lookup are sequential, offset by `address_initial_offset`.
         assert_eq!(
-            lookup_data.memory_lookups[0].data.len() * N_LANES,
+            (1 + interaction_prover.memory_values[0].len()) * N_LANES,
             1 << log_size
         );
-        for (i, addresses) in lookup_data.memory_lookups[0].data.iter().enumerate() {
+        for (i, addresses) in interaction_prover.memory_addresses.iter().enumerate() {
             assert_eq!(
                 addresses.to_array(),
-                array::from_fn(|j| {
-                    M31::from_u32_unchecked((i * N_LANES + j) as u32 + address_initial_offset)
-                })
+                array::from_fn(|j| { M31::from_u32_unchecked((i * N_LANES + j) as u32) })
             );
         }
     }
@@ -203,34 +276,48 @@ mod tests {
     fn test_generate_interaction_trace() {
         let mut rng = rand::thread_rng();
         let log_size = 8;
-        let address_initial_offset = 10032;
-        let inputs = (0..32)
-            .map(|i| RangeCheckInput {
-                address: PackedM31::from_array(array::from_fn(|j| {
+        let mem_log_size = log_size + 1;
+        let address_initial_offset = 256;
+        let inputs = (0..1 << (log_size - LOG_N_LANES))
+            .map(|i| {
+                PackedM31::from_array(array::from_fn(|j| {
                     M31::from_u32_unchecked(i * N_LANES as u32 + j as u32 + address_initial_offset)
-                })),
-                values: array::from_fn(|_| Simd::from_array(rng.gen())),
+                }))
             })
             .collect_vec();
 
-        let (trace, lookup_data) =
-            generate_trace(log_size, M31::from(address_initial_offset), &inputs);
+        let values = (0..1 << (mem_log_size - LOG_N_LANES))
+            .map(|_| {
+                array::from_fn(|i| {
+                    if i < 4 {
+                        Simd::from_array(rng.gen())
+                    } else {
+                        Simd::splat(0)
+                    }
+                })
+            })
+            .collect_vec();
+        let memory_trace_generator = MemoryClaimProver {
+            values: values.clone(),
+            multiplicities: vec![PackedUInt32::broadcast(0); 1 << (mem_log_size - LOG_N_LANES)],
+        };
+        let (trace, interaction_prover) = write_trace_simd(&inputs, &memory_trace_generator);
 
         let channel = &mut Blake2sChannel::new(Blake2sHasher::hash(BaseField::into_slice(&[])));
         let memory_lookup_elements = MemoryLookupElements::draw(channel);
         let range2_lookup_elements = RangeElements::draw(channel);
 
         let (interaction_trace, claimed_sum) = gen_interaction_trace(
+            &interaction_prover,
             log_size,
-            lookup_data,
             &memory_lookup_elements,
             &range2_lookup_elements,
         );
 
-        let trace = TreeVec::new(vec![trace, interaction_trace, vec![gen_is_first(log_size)]]);
+        let trace = TreeVec::new(vec![trace, interaction_trace]);
         let trace_polys = trace.map_cols(|c| c.interpolate());
 
-        let component = RangeCheck128BuiltinComponent {
+        let component = RangeCheckBuiltinComponent {
             log_size,
             initial_memory_address: M31::from(address_initial_offset),
             memory_lookup_elements,
