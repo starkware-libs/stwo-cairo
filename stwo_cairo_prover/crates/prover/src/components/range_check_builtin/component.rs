c37771f3df6fc246fe24b3159f2cf6b9dbc0f474
Alon Haramati <alonh@starkware.co>
2024-09-01 16:04:08 +0300

    Add last 2 bit value constraint.

--- stwo_cairo_prover/crates/prover/src/components/range_check_builtin/component.rs
+++ stwo_cairo_prover/crates/prover/src/components/range_check_builtin/component.rs
@@ -79,6 +79,17 @@ impl FrameworkEval for RangeCheckBuiltinEval {
             &self.memory_lookup_elements,
         );
 
+        // Add constraints for the last 2 bit value.
+        let last_value_felt = values[N_ADDRESS_FELTS + N_VALUES_FELTS - 1];
+        let tmp_value = eval.next_trace_mask();
+        eval.add_constraint(
+            tmp_value
+                - (last_value_felt
+                    * (last_value_felt - E::F::one())
+                    * (last_value_felt - E::F::from(M31::from(2)))),
+        );
+        eval.add_constraint(tmp_value * (last_value_felt - E::F::from(M31::from(3))));
+
         logup.finalize(&mut eval);
         eval
     }
@@ -98,7 +109,7 @@ impl RangeCheckBuiltinClaim {
     pub fn log_sizes(&self) -> TreeVec<Vec<u32>> {
         let n_values = self.memory_segment.end_addr - self.memory_segment.begin_addr;
         let log_size = n_values.next_power_of_two().ilog2();
-        let trace_log_sizes = vec![log_size; N_ADDRESS_FELTS + N_VALUES_FELTS];
+        let trace_log_sizes = vec![log_size; N_ADDRESS_FELTS + N_VALUES_FELTS + 1];
         let interaction_trace_log_sizes = vec![log_size; SECURE_EXTENSION_DEGREE];
         TreeVec::new(vec![trace_log_sizes, interaction_trace_log_sizes])
     }
