3f3589c66b6de52f8b88391cc3bf3bd2f07af74b
Yuval Goldberg <yuval@starkware.co>
2024-09-23 16:48:25 +0300

    Added adapted Stwo
    (
    Passed on generated input from alonh/stwo_cairo_input (by running in it `bazel run src/services/gps/tools/stwo_cairo_input:stwo_cairo_input`).
    Command line (but change to absolute paths!):
    ```
    cargo run --bin adapted_stwo -- --pub_json src/services/gps/tools/stwo_cairo_input/example_outputs/pub.json --priv_json src/services/gps/tools/stwo_cairo_input/example_outputs/priv.json
    ```
    )

---
+++ stwo_cairo_prover/crates/adapted_prover/src/main.rs
@@ -0,0 +1,49 @@
+use std::path::PathBuf;
+use std::process::ExitCode;
+
+use clap::Parser;
+use stwo_cairo_prover::cairo_air::{prove_cairo, CairoProof};
+use stwo_cairo_prover::input::vm_import::{import_from_vm_output, VmImportError};
+use stwo_cairo_prover::input::CairoInput;
+use stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleHasher;
+use thiserror::Error;
+
+/// Command line arguments for adapted_stwo.
+#[derive(Parser, Debug)]
+struct Args {
+    #[structopt(long = "pub_json")]
+    pub_json: PathBuf,
+    #[structopt(long = "priv_json")]
+    priv_json: PathBuf,
+}
+
+#[derive(Debug, Error)]
+enum Error {
+    #[error("Invalid arguments: {0}")]
+    Cli(#[from] clap::Error),
+    #[error("VM import failed: {0}")]
+    VmImport(#[from] VmImportError),
+}
+
+fn main() -> ExitCode {
+    match run(std::env::args()) {
+        Ok(_) => {
+            println!("Adapted prover succeeded");
+            ExitCode::SUCCESS
+        }
+        Err(error) => {
+            println!("Adapted prover failed: {error}");
+            ExitCode::FAILURE
+        }
+    }
+}
+
+fn run(args: impl Iterator<Item = String>) -> Result<CairoProof<Blake2sMerkleHasher>, Error> {
+    let args = Args::try_parse_from(args)?;
+
+    let vm_output: CairoInput =
+        import_from_vm_output(args.pub_json.as_path(), args.priv_json.as_path())?;
+    // TODO(yuval): consider changing to return Result.
+    // TODO(yuval): serialize the output to a file?
+    Ok(prove_cairo(vm_output))
+}
