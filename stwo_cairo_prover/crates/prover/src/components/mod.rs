2e5ad664a505dc7dba44064104dcf5f263b70adf
Ilya Lesokhin <ilya@starkware.co>
2024-11-12 08:55:58 +0200

    Bump stwo-prover version.

--- stwo_cairo_prover/crates/prover/src/components/mod.rs
+++ stwo_cairo_prover/crates/prover/src/components/mod.rs
@@ -1,3 +1,5 @@
+use stwo_prover::constraint_framework::INTERACTION_TRACE_IDX;
+
 pub mod memory;
 pub mod range_check_builtin;
 pub mod range_check_unit;
@@ -5,4 +7,4 @@ pub mod range_check_vector;
 pub mod ret_opcode;
 
 // TODO(ShaharS): Move to a common file.
-pub const LOOKUP_INTERACTION_PHASE: usize = 1;
+pub const LOOKUP_INTERACTION_PHASE: usize = INTERACTION_TRACE_IDX;
