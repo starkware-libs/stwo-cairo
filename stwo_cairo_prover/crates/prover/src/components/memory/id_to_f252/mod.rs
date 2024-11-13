6c4f481f392a9368334e1dae5051cba3168d3828
Ohad Agadi <ohad.a@starkware.co>
2024-11-13 08:23:56 +0200

    memory small table

--- stwo_cairo_prover/crates/prover/src/components/memory/id_to_f252/mod.rs
+++ stwo_cairo_prover/crates/prover/src/components/memory/id_to_f252/mod.rs
@@ -3,5 +3,7 @@ pub mod prover;
 
 pub const N_BITS_PER_FELT: usize = 9;
 
-pub use component::{Claim, Component, Eval, InteractionClaim, RelationElements};
-pub use prover::ClaimGenerator;
+pub use component::{
+    BigComponent, BigEval, Claim, InteractionClaim, RelationElements, SmallComponent, SmallEval,
+};
+pub use prover::{ClaimGenerator, InputType, PackedInputType};
