d1ec4a69eed65a9f205162977995356116f63d03
Ohad Agadi <ohad.a@starkware.co>
2024-11-20 12:55:15 +0200

    add generic_opcode

---
+++ stwo_cairo_prover/crates/prover/src/components/genericopcode/mod.rs
@@ -0,0 +1,6 @@
+#![allow(clippy::too_many_arguments)]
+pub mod component;
+pub mod prover;
+
+pub use component::{Claim, Component, Eval, InteractionClaim, RelationElements};
+pub use prover::{ClaimGenerator, InputType, InteractionClaimGenerator};
