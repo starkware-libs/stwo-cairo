4252a215a173056ed413f8c796398ef593eab8d1
Stavbe <stav@starkware.co>
2024-11-21 16:27:33 +0200

    add the opcode components to the adapter

--- stwo_cairo_prover/crates/prover/src/lib.rs
+++ stwo_cairo_prover/crates/prover/src/lib.rs
@@ -19,12 +19,174 @@ mod tests {
             call rel 2;
             [ap] = [ap-1] + 3;
             jmp abs [ap];
+            [ap] = 1, ap++;
         }
         .instructions;
 
-        let inp = input_from_plain_casm(instructions);
-        let instruction_counts = inp.instructions.counts();
-        assert_eq!(instruction_counts.jmp_abs[0], 1);
-        println!("Instruction counts: {instruction_counts:#?}");
+        let inp = input_from_plain_casm(instructions, false);
+        let vm_state_by_opcode = inp.state_transitions.vm_state_by_opcode;
+        assert_eq!(
+            vm_state_by_opcode
+                .jump_opcode_is_rel_f_is_imm_f_is_double_deref_f
+                .len(),
+            1
+        );
+        assert_eq!(
+            vm_state_by_opcode.call_opcode_is_rel_t_op1_base_fp_f.len(),
+            1
+        );
+        assert_eq!(vm_state_by_opcode.add_opcode_is_small_t_is_imm_t.len(), 1);
+    }
+
+    #[test]
+    fn test_add_ap() {
+        let instructions = casm! {
+            [ap] = 38, ap++;
+            [ap] = 12, ap++;
+            ap += [ap -2];
+            ap += [fp + 1];
+            [ap] = 1, ap++;
+        }
+        .instructions;
+
+        let inp = input_from_plain_casm(instructions, false);
+        let vm_state_by_opcode = inp.state_transitions.vm_state_by_opcode;
+        assert_eq!(
+            vm_state_by_opcode
+                .add_ap_opcode_is_imm_f_op1_base_fp_f
+                .len(),
+            1
+        );
+        assert_eq!(
+            vm_state_by_opcode
+                .add_ap_opcode_is_imm_f_op1_base_fp_f
+                .len(),
+            1
+        );
+    }
+
+    #[test]
+    fn test_call() {
+        let instructions = casm! {
+            call rel 2;
+            call abs [fp - 1];
+            [ap] = 1, ap++;
+        }
+        .instructions;
+
+        let inp = input_from_plain_casm(instructions, false);
+        let vm_state_by_opcode = inp.state_transitions.vm_state_by_opcode;
+        assert_eq!(
+            vm_state_by_opcode.call_opcode_is_rel_f_op1_base_fp_t.len(),
+            2
+        );
+        assert_eq!(
+            vm_state_by_opcode.call_opcode_is_rel_t_op1_base_fp_f.len(),
+            1
+        );
+    }
+
+    #[test]
+    fn test_call2() {
+        let instructions = casm! {
+            call rel 2;
+            call abs [ap - 1];
+            [ap] = 1, ap++;
+        }
+        .instructions;
+
+        let inp = input_from_plain_casm(instructions, false);
+        let vm_state_by_opcode = inp.state_transitions.vm_state_by_opcode;
+        assert_eq!(
+            vm_state_by_opcode.call_opcode_is_rel_f_op1_base_fp_f.len(),
+            2
+        );
+    }
+
+    #[test]
+    fn test_jnz_taken() {
+        let instructions = casm! {
+            [ap] = 0, ap++;
+            jmp rel 2 if [ap-1] != 0;
+            [ap] = 1, ap++;
+        }
+        .instructions;
+
+        let inp = input_from_plain_casm(instructions, false);
+        let vm_state_by_opcode = inp.state_transitions.vm_state_by_opcode;
+        assert_eq!(
+            vm_state_by_opcode.jnz_opcode_is_taken_f_dst_base_fp_f.len(),
+            1
+        );
+    }
+
+    #[test]
+    fn test_jnz_not_taken() {
+        let instructions = casm! {
+            call rel 2;
+            jmp rel 2 if [fp-1] != 0;
+            [ap] = 1, ap++;
+        }
+        .instructions;
+
+        let inp = input_from_plain_casm(instructions, false);
+        let vm_state_by_opcode = inp.state_transitions.vm_state_by_opcode;
+        assert_eq!(
+            vm_state_by_opcode.jnz_opcode_is_taken_t_dst_base_fp_t.len(),
+            1
+        );
+    }
+
+    #[test]
+    fn test_assert_equal() {
+        let instructions = casm! {
+            [ap] =  8, ap++;
+            [ap] =  8, ap++;
+            [ap+2] = [fp + 1];
+            [ap] = 1, ap++;
+        }
+        .instructions;
+
+        let inp = input_from_plain_casm(instructions, false);
+        let vm_state_by_opcode = inp.state_transitions.vm_state_by_opcode;
+        assert_eq!(
+            vm_state_by_opcode
+                .assert_eq_opcode_is_double_deref_f_is_imm_f
+                .len(),
+            1
+        );
+    }
+
+    #[test]
+    fn test_add() {
+        let instructions = casm! {
+            call rel 2;
+            [ap] =  8, ap++;
+            [ap] = 12, ap++;
+            [ap+2] = [fp-1] + [ap-2];
+            [ap] = 1, ap++;
+        }
+        .instructions;
+
+        let inp = input_from_plain_casm(instructions, false);
+        let vm_state_by_opcode = inp.state_transitions.vm_state_by_opcode;
+        assert_eq!(vm_state_by_opcode.add_opcode_is_small_t_is_imm_f.len(), 1);
+    }
+
+    #[test]
+    fn test_mul() {
+        let instructions = casm! {
+            [ap] =  8, ap++;
+            [ap] = 12, ap++;
+            [ap] = [ap-1] * [ap-2], ap++;
+            [ap] = [ap-1]*7, ap++;
+            [ap] = 1, ap++;
+        }
+        .instructions;
+
+        let inp = input_from_plain_casm(instructions, false);
+        let vm_state_by_opcode = inp.state_transitions.vm_state_by_opcode;
+        assert_eq!(vm_state_by_opcode.mul_opcode_is_small_t_is_imm_f.len(), 1);
+        assert_eq!(vm_state_by_opcode.mul_opcode_is_small_t_is_imm_t.len(), 1);
     }
 }
