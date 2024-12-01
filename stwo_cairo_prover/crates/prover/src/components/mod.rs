202ef2913f7a5ae8e3ffd3e34960a299fd9723b2
Ohad Agadi <ohad.a@starkware.co>
2024-12-01 17:23:09 +0200

    update verify generic and ret from auto gen

--- stwo_cairo_prover/crates/prover/src/components/mod.rs
+++ stwo_cairo_prover/crates/prover/src/components/mod.rs
@@ -10,6 +10,10 @@ pub mod verify_instruction;
 pub use memory::{memory_address_to_id, memory_id_to_big};
 pub use range_check_vector::{range_check_19, range_check_4_3, range_check_7_2_5, range_check_9_9};
 
+// [`SubComponentInputs::bit_reverse_coset_to_circle_domain_order`] permutes the
+// bit-reversed-canonic-coset ordered values to the natural order. Used when padding is done, to
+// ignore padded values at the tail of the natural ordered vectored .
+
 pub fn pack_values<T: Pack>(values: &[T]) -> Vec<T::SimdType> {
     values
         .array_chunks::<N_LANES>()
