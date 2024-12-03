63d28900ad36c7ef224c7231cfc186948b34c215
Alon Titelman <alon.titelman@starkware.co>
2024-12-03 10:13:51 +0200

    Generic opcode repr test.

--- stwo_cairo_prover/crates/prover/src/components/genericopcode/mod.rs
+++ stwo_cairo_prover/crates/prover/src/components/genericopcode/mod.rs
@@ -4,3 +4,31 @@ pub mod prover;
 
 pub use component::{Claim, Component, Eval, InteractionClaim, RelationElements};
 pub use prover::{ClaimGenerator, InputType, InteractionClaimGenerator};
+
+#[cfg(test)]
+mod tests {
+    use stwo_prover::constraint_framework::expr::ExprEvaluator;
+    use stwo_prover::constraint_framework::FrameworkEval;
+
+    use crate::components::genericopcode;
+    use crate::relations;
+
+    #[test]
+    fn test_generic_opcode_repr() {
+        let eval = genericopcode::Eval {
+            claim: genericopcode::Claim { n_calls: 0 },
+            memoryaddresstoid_lookup_elements: relations::AddrToId::dummy(),
+            memoryidtobig_lookup_elements: relations::IdToValue::dummy(),
+            range_check_19_lookup_elements: relations::RangeCheck_19::dummy(),
+            range_check_9_9_lookup_elements: relations::RangeCheck_9_9::dummy(),
+            verifyinstruction_lookup_elements: relations::VerifyInstruction::dummy(),
+            opcodes_lookup_elements: relations::Vm::dummy(),
+        };
+
+        let expected = "";
+        let constraint_str = eval
+            .evaluate(ExprEvaluator::new(16, false))
+            .format_constraints();
+        assert_eq!(constraint_str, expected);
+    }
+}
