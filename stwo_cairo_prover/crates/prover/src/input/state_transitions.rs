8afd63dff8ed4ae3258438b186832612d2ca3ac2
Ohad Agadi <ohad.a@starkware.co>
2024-12-03 13:29:38 +0200

    jump

--- stwo_cairo_prover/crates/prover/src/input/state_transitions.rs
+++ stwo_cairo_prover/crates/prover/src/input/state_transitions.rs
@@ -289,7 +289,7 @@ impl StateTransitions {
                 opcode_call: false,
                 opcode_ret: false,
                 opcode_assert_eq: false,
-            } if !dev_mode => {
+            } => {
                 if op1_imm {
                     // jump rel imm.
                     assert!(
