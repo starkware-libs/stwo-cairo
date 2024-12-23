52446a37b274b3063f227e5759177eb9e451e473
Stav Beno <stav@starkware.co>
2024-12-23 16:07:19 +0200

    unify adapter functionality

--- stwo_cairo_prover/crates/adapted_prover/src/main.rs
+++ stwo_cairo_prover/crates/adapted_prover/src/main.rs
@@ -4,7 +4,7 @@ use std::process::ExitCode;
 use clap::Parser;
 use stwo_cairo_prover::cairo_air::air::CairoProof;
 use stwo_cairo_prover::cairo_air::prove_cairo;
-use stwo_cairo_prover::input::vm_import::{import_from_vm_output, VmImportError};
+use stwo_cairo_prover::input::vm_import::{adapt_vm_output, VmImportError};
 use stwo_cairo_prover::input::CairoInput;
 use stwo_cairo_utils::logging_utils::init_logging;
 use stwo_prover::core::prover::ProvingError;
@@ -66,7 +66,7 @@ fn run(args: impl Iterator<Item = String>) -> Result<CairoProof<Blake2sMerkleHas
     let args = Args::try_parse_from(args)?;
 
     let vm_output: CairoInput =
-        import_from_vm_output(args.pub_json.as_path(), args.priv_json.as_path(), true)?;
+        adapt_vm_output(args.pub_json.as_path(), args.priv_json.as_path(), true)?;
 
     let casm_states_by_opcode_count = &vm_output.state_transitions.casm_states_by_opcode.counts();
     log::info!("Casm states by opcode count: {casm_states_by_opcode_count:?}");
