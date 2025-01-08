7eddb1af422ac425324a14c9df96f12f1e989b40
ohad-starkware <ohad.a@starkware.co>
2025-01-08 11:09:55 +0200

    instruction cache outside of statetransitions

--- stwo_cairo_prover/crates/prover/src/input/mod.rs
+++ stwo_cairo_prover/crates/prover/src/input/mod.rs
@@ -1,9 +1,12 @@
+use std::collections::HashMap;
+
 use builtin_segments::BuiltinSegments;
 use memory::Memory;
+use prover_types::cpu::M31;
 use state_transitions::StateTransitions;
 
 pub mod builtin_segments;
-mod decode;
+pub mod decode;
 pub mod memory;
 pub mod plain;
 pub mod range_check_unit;
@@ -16,6 +19,7 @@ pub const N_REGISTERS: usize = 3;
 #[derive(Debug)]
 pub struct ProverInput {
     pub state_transitions: StateTransitions,
+    pub instruction_by_pc: HashMap<M31, u64>,
     pub memory: Memory,
     pub public_memory_addresses: Vec<u32>,
     pub builtins_segments: BuiltinSegments,
