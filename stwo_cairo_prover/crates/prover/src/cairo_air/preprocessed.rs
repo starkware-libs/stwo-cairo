1e9b8a7538d09ed64ca42f2d23913e4bec01f842
Shahar Samocha <shahars@starkware.co>
2025-01-27 12:09:14 +0200

    Remove IsFirst preprocessed column

--- stwo_cairo_prover/crates/prover/src/cairo_air/preprocessed.rs
+++ stwo_cairo_prover/crates/prover/src/cairo_air/preprocessed.rs
@@ -1,6 +1,6 @@
-use itertools::{chain, Itertools};
+use itertools::Itertools;
 use prover_types::simd::LOG_N_LANES;
-use stwo_prover::constraint_framework::preprocessed_columns::{IsFirst, PreProcessedColumnId};
+use stwo_prover::constraint_framework::preprocessed_columns::PreProcessedColumnId;
 use stwo_prover::core::backend::simd::m31::{PackedM31, N_LANES};
 use stwo_prover::core::backend::simd::SimdBackend;
 use stwo_prover::core::backend::Col;
@@ -12,9 +12,6 @@ use super::LOG_MAX_ROWS;
 use crate::components::range_check_vector::SIMD_ENUMERATION_0;
 
 const N_PREPROCESSED_COLUMN_SIZES: usize = (LOG_MAX_ROWS - LOG_N_LANES) as usize + 1;
-
-// List of sizes to initialize the preprocessed trace with for `PreprocessedColumn::IsFirst`.
-const IS_FIRST_LOG_SIZES: [u32; N_PREPROCESSED_COLUMN_SIZES] = preprocessed_log_sizes();
 
 // List of sizes to initialize the preprocessed trace with for `PreprocessedColumn::Seq`.
 const SEQ_LOG_SIZES: [u32; N_PREPROCESSED_COLUMN_SIZES] = preprocessed_log_sizes();
@@ -31,25 +28,21 @@ const fn preprocessed_log_sizes() -> [u32; N_PREPROCESSED_COLUMN_SIZES] {
 }
 
 pub enum PreProcessedColumn {
-    IsFirst(IsFirst),
     Seq(Seq),
 }
 impl PreProcessedColumn {
     pub fn log_size(&self) -> u32 {
         match self {
-            PreProcessedColumn::IsFirst(column) => column.log_size,
             PreProcessedColumn::Seq(column) => column.log_size,
         }
     }
     pub fn id(&self) -> PreProcessedColumnId {
         match self {
-            PreProcessedColumn::IsFirst(column) => column.id(),
             PreProcessedColumn::Seq(column) => column.id(),
         }
     }
     pub fn gen_column_simd(&self) -> CircleEvaluation<SimdBackend, BaseField, BitReversedOrder> {
         match self {
-            PreProcessedColumn::IsFirst(column) => column.gen_column_simd(),
             PreProcessedColumn::Seq(column) => column.gen_column_simd(),
         }
     }
@@ -57,10 +50,9 @@ impl PreProcessedColumn {
 
 /// Returns column info for the preprocessed trace.
 pub fn preprocessed_trace_columns() -> Vec<PreProcessedColumn> {
-    let is_first_columns =
-        IS_FIRST_LOG_SIZES.map(|log_size| PreProcessedColumn::IsFirst(IsFirst::new(log_size)));
     let seq_columns = SEQ_LOG_SIZES.map(|log_size| PreProcessedColumn::Seq(Seq::new(log_size)));
-    chain![is_first_columns, seq_columns]
+    seq_columns
+        .into_iter()
         .sorted_by_key(|column| std::cmp::Reverse(column.log_size()))
         .collect_vec()
 }
