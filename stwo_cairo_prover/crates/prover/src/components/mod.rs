2b35ac531795a4074eba657aad51171b26540bc2
Dan Carmon <dancar@starkware.co>
2025-02-03 17:07:49 +0200

    wip deduce_output implementations

--- stwo_cairo_prover/crates/prover/src/components/mod.rs
+++ stwo_cairo_prover/crates/prover/src/components/mod.rs
@@ -21,6 +21,7 @@ pub mod jump_opcode_double_deref;
 pub mod jump_opcode_rel;
 pub mod jump_opcode_rel_imm;
 pub mod memory;
+pub mod poseidon_deductions;
 pub mod range_check_builtin_bits_128;
 pub mod range_check_builtin_bits_96;
 pub mod range_check_vector;
