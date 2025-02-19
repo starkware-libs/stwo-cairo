9ccafc8aac1750b106dacf1facef3a7a0c127513
Gali Michlevich <gali@starkware.co>
2025-02-19 17:34:30 +0200

    deduce_output for Poseidon Rounds

--- stwo_cairo_prover/crates/prover/src/components/poseidon_3_partial_rounds_chain/prover.rs
+++ stwo_cairo_prover/crates/prover/src/components/poseidon_3_partial_rounds_chain/prover.rs
@@ -1,7 +1,9 @@
 #![allow(unused_parens)]
 use itertools::Itertools;
+use stwo_prover::core::backend::simd::conversion::Pack;
 
 use super::component::{Claim, InteractionClaim};
+use crate::cairo_air::poseidon::deduce_output::Poseidon3PartialRoundsChain;
 use crate::components::prelude::proving::*;
 use crate::components::range_check_vector::{range_check_4_4, range_check_4_4_4_4};
 use crate::components::{cube_252, poseidon_round_keys, range_check_felt_252_width_27};
@@ -65,6 +67,37 @@ impl ClaimGenerator {
 
     pub fn add_inputs(&mut self, inputs: &[InputType]) {
         self.inputs.extend_from_slice(inputs);
+    }
+
+    pub fn deduce_output(&self, input: PackedInputType) -> PackedInputType {
+        let unpacked_inputs = (
+            input.0.to_array(),
+            input.1.to_array(),
+            <_ as Unpack>::unpack(input.2),
+        );
+
+        let mut unpacked_deduced_outputs: (
+            [M31; N_LANES],
+            [M31; N_LANES],
+            [[Felt252Width27; 4]; N_LANES],
+        ) = Default::default();
+        (0..N_LANES).for_each(|i| {
+            (
+                unpacked_deduced_outputs.0[i],
+                unpacked_deduced_outputs.1[i],
+                unpacked_deduced_outputs.2[i],
+            ) = Poseidon3PartialRoundsChain::deduce_output(
+                unpacked_inputs.0[i],
+                unpacked_inputs.1[i],
+                unpacked_inputs.2[i],
+            )
+        });
+
+        (
+            PackedM31::from_array(unpacked_deduced_outputs.0),
+            PackedM31::from_array(unpacked_deduced_outputs.0),
+            <_ as Pack>::pack(unpacked_deduced_outputs.2),
+        )
     }
 }
 
