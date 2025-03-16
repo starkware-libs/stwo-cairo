cd2da3af9cae84091f56861f192a4b73513cd9e4
ohad nir <ohad.n@starkware.co>
2025-03-16 16:45:19 +0200

    add pedersen builtin and the components it requires

--- stwo_cairo_prover/crates/prover/src/cairo_air/relations.rs
+++ stwo_cairo_prover/crates/prover/src/cairo_air/relations.rs
@@ -9,6 +9,7 @@ relation!(PoseidonFullRoundChain, 32);
 relation!(Poseidon3PartialRoundsChain, 42);
 relation!(PoseidonRoundKeys, 31);
 relation!(RangeCheck_6, 1);
+relation!(RangeCheck_8, 1);
 relation!(RangeCheck_11, 1);
 relation!(RangeCheck_12, 1);
 relation!(RangeCheck_18, 1);
@@ -16,6 +17,7 @@ relation!(RangeCheck_19, 1);
 relation!(RangeCheck_3_6, 2);
 relation!(RangeCheck_4_3, 2);
 relation!(RangeCheck_4_4, 2);
+relation!(RangeCheck_5_4, 2);
 relation!(RangeCheck_9_9, 2);
 relation!(RangeCheck_7_2_5, 3);
 relation!(RangeCheck_3_6_6_3, 4);
@@ -32,3 +34,5 @@ relation!(BlakeG, 20);
 relation!(TripleXor32, 3);
 relation!(BlakeRound, 35);
 relation!(BlakeRoundSigma, 99);
+relation!(PartialEcMul, 73); //
+relation!(PedersenPointsTable, 57); //
