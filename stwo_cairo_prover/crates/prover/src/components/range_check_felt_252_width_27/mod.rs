77baf635e700adb92254703ccaf3e045dac3a27e
Gali Michlevich <gali@starkware.co>
2025-03-17 17:32:52 +0200

    rc felt252width27 files

---
+++ stwo_cairo_prover/crates/prover/src/components/range_check_felt_252_width_27/mod.rs
@@ -0,0 +1,6 @@
+// TODO(Gali): remove unused.
+#![allow(unused)]
+pub mod component;
+pub mod prover;
+pub use component::{Claim, Component, Eval, InteractionClaim};
+pub use prover::{ClaimGenerator, InputType, InteractionClaimGenerator};
