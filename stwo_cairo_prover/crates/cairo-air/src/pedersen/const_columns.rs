92c30384e87f16c358d8bdfd9a10b325497be558
ohad nir <ohad.n@starkware.co>
2025-03-23 15:16:28 +0200

    deduce_output of pedersen_points_table

--- stwo_cairo_prover/crates/cairo-air/src/pedersen/const_columns.rs
+++ stwo_cairo_prover/crates/cairo-air/src/pedersen/const_columns.rs
@@ -24,8 +24,7 @@ use stwo_prover::core::poly::BitReversedOrder;
 use super::utils::felt_batch_inverse;
 use crate::preprocessed::PreProcessedColumn;
 
-pub(super) static PEDERSEN_TABLE: LazyLock<PedersenPointsTable> =
-    LazyLock::new(PedersenPointsTable::new);
+pub static PEDERSEN_TABLE: LazyLock<PedersenPointsTable> = LazyLock::new(PedersenPointsTable::new);
 pub const PEDERSEN_TABLE_N_COLUMNS: usize = FELT252_N_WORDS * 2;
 
 #[derive(Debug)]
@@ -72,7 +71,7 @@ impl PreProcessedColumn for PedersenPoints {
 // 2. Next 16 rows: Row k contains -P_shift + k * P_1
 // 3. Next 14 blocks of 2 ** 18 rows: Row k of block b contains -P_shift + 2**(18*b) * k * P_2
 // 4. Next 16 rows: Row k contains -P_shift + k * P_3
-pub(super) struct PedersenPointsTable {
+pub struct PedersenPointsTable {
     // The one copy of the column contents. Shared by all column instances.
     column_data: [Vec<BaseField>; PEDERSEN_TABLE_N_COLUMNS],
 
@@ -83,6 +82,12 @@ impl PedersenPointsTable {
     #[allow(dead_code)] //  Will be used by the deduce_output of PartialEcMul
     pub fn get_row(&self, index: usize) -> AffinePoint {
         self.rows[index].clone()
+    }
+
+    pub fn get_row_coordinates(&self, index: usize) -> [Felt252; 2] {
+        let x_f252: Felt252 = PEDERSEN_TABLE.rows[index].x().into();
+        let y_f252: Felt252 = PEDERSEN_TABLE.rows[index].y().into();
+        [x_f252, y_f252]
     }
 
     fn new() -> Self {
