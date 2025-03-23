fcf939f328f9caa1760faa68a3d50c0497de82c5
ohad-starkware <ohad.a@starkware.co>
2025-03-23 16:25:00 +0200

    xor12 expand bits

--- stwo_cairo_prover/crates/prover/src/components/verify_bitwise_xor_12/mod.rs
+++ stwo_cairo_prover/crates/prover/src/components/verify_bitwise_xor_12/mod.rs
@@ -1,6 +1,13 @@
 pub mod component;
 pub mod prover;
 
-// TODO(Ohad): expand bits.
+// TODO(Ohad): generalize, test.
 pub use component::{Claim, Component, Eval, InteractionClaim};
 pub use prover::{ClaimGenerator, InteractionClaimGenerator};
+
+const ELEM_BITS: u32 = 12;
+const EXPAND_BITS: u32 = 2;
+const LIMB_BITS: u32 = ELEM_BITS - EXPAND_BITS;
+const LOG_SIZE: u32 = (ELEM_BITS - EXPAND_BITS) * 2;
+const N_MULT_COLUMNS: usize = 1 << (EXPAND_BITS * 2);
+const N_TRACE_COLUMNS: usize = N_MULT_COLUMNS;
