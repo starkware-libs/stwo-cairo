fa87441dc869f95a1c03eeb08b5ff1760c4483f0
Ohad Nir <141617878+ohad-nir-starkware@users.noreply.github.com>
2025-04-01 17:25:44 +0300

    implement the packed version of deduce_output for partial_ec_mul (#666)

--- stwo_cairo_prover/crates/common/src/prover_types/simd.rs
+++ stwo_cairo_prover/crates/common/src/prover_types/simd.rs
@@ -685,6 +685,14 @@ impl Pack for Felt252 {
     }
 }
 
+impl Unpack for PackedFelt252 {
+    type CpuType = Felt252;
+
+    fn unpack(self) -> [Self::CpuType; N_LANES] {
+        self.to_array()
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use rand::rngs::SmallRng;
