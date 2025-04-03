ea8f2b362eb65db6935ca765f098a46f36d3a345
ohad-starkware <ohad.a@starkware.co>
2025-04-03 15:50:17 +0300

    set min stack size

--- stwo_cairo_prover/crates/prover/src/prover.rs
+++ stwo_cairo_prover/crates/prover/src/prover.rs
@@ -19,6 +19,10 @@ use crate::witness::utils::witness_trace_cells;
 
 pub(crate) const LOG_MAX_ROWS: u32 = 26;
 
+// Stack-overflows induced by the partial_ec_mul write trace function.
+// Decreasing this value may cause a stack-overflow during witness generation.
+const RAYON_THREAD_STACK_SIZE: usize = 4194304;
+
 /// Logup security is defined by the `QM31` space (~124 bits) +
 /// `INTERACTION_POW_BITS` over total number of relation terms. E.g. assuming a 100-bit
 /// security target, the witness may contain up to 1 << (24 + INTERACTION_POW_BITS) relation
@@ -33,6 +37,11 @@ pub fn prove_cairo<MC: MerkleChannel>(
 where
     SimdBackend: BackendForChannel<MC>,
 {
+    // TODO(Ohad): remove when un-inlining the witness generation is done.
+    rayon::ThreadPoolBuilder::new()
+        .stack_size(RAYON_THREAD_STACK_SIZE)
+        .build_global()
+        .unwrap();
     let _span = span!(Level::INFO, "prove_cairo").entered();
     // Composition polynomial domain log size is LOG_MAX_ROWS + 1, double it
     // because we compute on a half-coset, and account for blowup factor.
