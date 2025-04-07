a2ca190a69b2610eff223c63e0e16dd737ba089c
Alon Frydberg <alonf@starkware.co>
2025-04-07 16:00:48 +0300

    Soundness: Verify builtin info, and the start of the program

--- stwo_cairo_prover/crates/cairo-air/src/verifier.rs
+++ stwo_cairo_prover/crates/cairo-air/src/verifier.rs
@@ -1,5 +1,10 @@
 use num_traits::Zero;
+use stwo_cairo_adapter::builtins::{
+    ADD_MOD_MEMORY_CELLS, BITWISE_MEMORY_CELLS, MUL_MOD_MEMORY_CELLS, PEDERSEN_MEMORY_CELLS,
+    POSEIDON_MEMORY_CELLS, RANGE_CHECK_MEMORY_CELLS,
+};
 use stwo_cairo_common::memory::LOG_MEMORY_ADDRESS_BOUND;
+use stwo_cairo_common::prover_types::cpu::CasmState;
 use stwo_prover::constraint_framework::PREPROCESSED_TRACE_IDX;
 use stwo_prover::core::channel::{Channel, MerkleChannel};
 use stwo_prover::core::fields::qm31::SecureField;
@@ -7,9 +12,154 @@ use stwo_prover::core::pcs::{CommitmentSchemeVerifier, PcsConfig};
 use stwo_prover::core::prover::{verify, VerificationError};
 use thiserror::Error;
 
-use crate::air::{lookup_sum, CairoComponents, CairoInteractionElements};
+use crate::air::{
+    lookup_sum, CairoClaim, CairoComponents, CairoInteractionElements, MemorySection, PublicData,
+    PublicMemory, PublicSegmentRanges,
+};
+use crate::builtins_air::BuiltinsClaim;
 use crate::components::memory_address_to_id::MEMORY_ADDRESS_TO_ID_SPLIT;
 use crate::{CairoProof, PreProcessedTraceVariant};
+
+fn verify_builtin_usage_boundaries(
+    builtins_claim: &BuiltinsClaim,
+    segment_ranges: &PublicSegmentRanges,
+) {
+    // Check that all non-supported builtins aren't used.
+    assert_eq!(
+        segment_ranges.ec_op.start_ptr.value,
+        segment_ranges.ec_op.stop_ptr.value
+    );
+    assert_eq!(
+        segment_ranges.ecdsa.start_ptr.value,
+        segment_ranges.ecdsa.stop_ptr.value
+    );
+    assert_eq!(
+        segment_ranges.keccak.start_ptr.value,
+        segment_ranges.keccak.stop_ptr.value
+    );
+
+    // Check that output start and end pointer make sense.
+    assert!(
+        segment_ranges.output.stop_ptr.value <= 1 << 27,
+        "Memory cannot reach beyond 2^27"
+    );
+    assert!(segment_ranges.output.start_ptr.value <= segment_ranges.output.stop_ptr.value);
+
+    // `builtins_info` an iterable of tuples containing the builtin segment start, log size, number
+    // of memory cells per builtin instance, and the builtin usage range.
+    let builtins_info = [
+        builtins_claim.range_check_128_builtin.map(|claim| {
+            (
+                claim.range_check_builtin_segment_start,
+                claim.log_size,
+                RANGE_CHECK_MEMORY_CELLS,
+                segment_ranges.range_check_128,
+            )
+        }),
+        builtins_claim.bitwise_builtin.map(|claim| {
+            (
+                claim.bitwise_builtin_segment_start,
+                claim.log_size,
+                BITWISE_MEMORY_CELLS,
+                segment_ranges.bitwise,
+            )
+        }),
+        builtins_claim.range_check_96_builtin.map(|claim| {
+            (
+                claim.range_check96_builtin_segment_start,
+                claim.log_size,
+                RANGE_CHECK_MEMORY_CELLS,
+                segment_ranges.range_check_96,
+            )
+        }),
+        builtins_claim.add_mod_builtin.map(|claim| {
+            (
+                claim.add_mod_builtin_segment_start,
+                claim.log_size,
+                ADD_MOD_MEMORY_CELLS,
+                segment_ranges.add_mod,
+            )
+        }),
+        builtins_claim.mul_mod_builtin.map(|claim| {
+            (
+                claim.mul_mod_builtin_segment_start,
+                claim.log_size,
+                MUL_MOD_MEMORY_CELLS,
+                segment_ranges.mul_mod,
+            )
+        }),
+        builtins_claim.pedersen_builtin.map(|claim| {
+            (
+                claim.pedersen_builtin_segment_start,
+                claim.log_size,
+                PEDERSEN_MEMORY_CELLS,
+                segment_ranges.pedersen,
+            )
+        }),
+        builtins_claim.poseidon_builtin.map(|claim| {
+            (
+                claim.poseidon_builtin_segment_start,
+                claim.log_size,
+                POSEIDON_MEMORY_CELLS,
+                segment_ranges.poseidon,
+            )
+        }),
+    ]
+    .into_iter()
+    .flatten();
+
+    for (start, log_size, n_cells, range) in builtins_info {
+        assert!(
+            range.stop_ptr.value <= 1 << 27,
+            "Memory cannot reach beyond 2^27"
+        );
+        assert!(range.start_ptr.value <= range.stop_ptr.value);
+        assert_eq!(range.start_ptr.value, start);
+        assert!(
+            range.stop_ptr.value <= start + (1 << log_size) * n_cells as u32,
+            "Builtin stop pointer must be within the builtin segment"
+        );
+    }
+}
+
+fn verify_program(program: &MemorySection) {
+    assert_eq!(program[0].1, [0x7fff7fff, 0x4078001, 0, 0, 0, 0, 0, 0]); // Instruction: ap += N_BUILTINS.
+    assert_eq!(program[1].1, [11, 0, 0, 0, 0, 0, 0, 0]); // Immediate of last instruction, should equal 11.
+    assert_eq!(program[2].1, [0x80018000, 0x11048001, 0, 0, 0, 0, 0, 0]); // Instruction: call rel ?.
+    assert_eq!(program[4].1, [0x7fff7fff, 0x1078001, 0, 0, 0, 0, 0, 0]); // Instruction: call rel ?.
+    assert_eq!(program[5].1, [0, 0, 0, 0, 0, 0, 0, 0]); // Instruction: call rel ?.
+}
+fn verify_public_input(claim: &CairoClaim) {
+    let PublicData {
+        public_memory:
+            PublicMemory {
+                program,
+                public_segments,
+                output: _output,
+                safe_call: _safe_call,
+            },
+        initial_state:
+            CasmState {
+                pc: initial_pc,
+                ap: initial_ap,
+                fp: _initial_fp,
+            },
+        final_state:
+            CasmState {
+                pc: final_pc,
+                ap: final_ap,
+                fp: _final_fp,
+            },
+    } = &claim.public_data;
+
+    verify_builtin_usage_boundaries(&claim.builtins, public_segments);
+
+    verify_program(program);
+
+    assert_eq!(initial_pc.0, 1);
+    assert_eq!(final_pc.0, 5);
+    assert!(initial_ap <= final_ap);
+}
 
 pub fn verify_cairo<MC: MerkleChannel>(
     CairoProof {
@@ -27,6 +177,8 @@ pub fn verify_cairo<MC: MerkleChannel>(
         (1 << claim.memory_address_to_id.log_size) * MEMORY_ADDRESS_TO_ID_SPLIT
             <= (1 << LOG_MEMORY_ADDRESS_BOUND)
     );
+
+    verify_public_input(&claim);
 
     let channel = &mut MC::C::default();
     let commitment_scheme_verifier = &mut CommitmentSchemeVerifier::<MC>::new(pcs_config);
