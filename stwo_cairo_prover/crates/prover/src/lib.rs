43596047eb80bdd09fa92f85a6fa7de7dd9b133e
ohad-starkware <ohad.a@starkware.co>
2025-04-10 17:49:30 +0300

    changed blake schedualing to official logic

--- stwo_cairo_prover/crates/prover/src/lib.rs
+++ stwo_cairo_prover/crates/prover/src/lib.rs
@@ -1,10 +1,4 @@
-#![feature(
-    trait_upcasting,
-    portable_simd,
-    iter_array_chunks,
-    array_chunks,
-    raw_slice_split
-)]
+#![feature(portable_simd, iter_array_chunks, array_chunks, raw_slice_split)]
 // TODO(Ohad): remove.
 #![allow(clippy::too_many_arguments, clippy::module_inception)]
 
