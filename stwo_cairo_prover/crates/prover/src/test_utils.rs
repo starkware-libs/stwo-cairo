c2831be1ab4547fb23c0658b3b8ea177a46c34d0
Stav Beno <stav@starkware.co>
2025-04-16 17:37:04 +0300

    add golden test to the adapter

--- stwo_cairo_prover/crates/prover/src/test_utils.rs
+++ stwo_cairo_prover/crates/prover/src/test_utils.rs
@@ -1,9 +1,12 @@
-use std::path::Path;
+use std::fs::{read_to_string, File};
+use std::io::Write;
+use std::path::{Path, PathBuf};
 
 use cairo_vm::cairo_run::{cairo_run, CairoRunConfig};
 use cairo_vm::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::*;
 use cairo_vm::types::layout_name::LayoutName;
 use cairo_vm::vm::runners::cairo_runner::CairoRunner;
+use serde_json::{to_string_pretty, Value};
 use stwo_cairo_adapter::plain::adapt_finished_runner;
 use stwo_cairo_adapter::ProverInput;
 
@@ -40,4 +43,21 @@ pub fn runner_from_compiled_cairo_program(test_name: &str) -> CairoRunner {
 pub fn prover_input_from_compiled_cairo_program(file_name: &str) -> ProverInput {
     let runner = runner_from_compiled_cairo_program(file_name);
     adapt_finished_runner(runner).expect("Unable to create prover input from finished runner")
+}
+
+pub fn get_prover_input_path(test_name: &str) -> PathBuf {
+    PathBuf::from(env!("CARGO_MANIFEST_DIR"))
+        .join("../../test_data/")
+        .join(test_name)
+        .join("prover_input.json")
+}
+
+pub fn read_json(file_path: &PathBuf) -> Value {
+    let json_file = read_to_string(file_path).unwrap();
+    serde_json::from_str(&json_file).expect("Invalid JSON file")
+}
+
+pub fn write_json(file_path: &PathBuf, value: &Value) {
+    let mut file = File::create(file_path).unwrap();
+    write!(file, "{}", &to_string_pretty(&value).unwrap()).unwrap();
 }
