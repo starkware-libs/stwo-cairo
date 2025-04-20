165b07a707b6f6669551ab9ae4f80115798ac6b0
Stav Beno <stav@starkware.co>
2025-04-20 11:19:12 +0300

    change layout to all_cairo_stwo in tests

--- stwo_cairo_prover/crates/adapter/src/opcodes.rs
+++ stwo_cairo_prover/crates/adapter/src/opcodes.rs
@@ -623,16 +623,38 @@ fn is_small_mul(op0: MemoryValue, op_1: MemoryValue) -> bool {
 mod mappings_tests {
 
     use cairo_lang_casm::casm;
+    use cairo_vm::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
+    use cairo_vm::types::layout_name::LayoutName;
+    use cairo_vm::vm::runners::cairo_runner::CairoRunner;
     use stwo_cairo_common::prover_types::cpu::CasmState;
     use stwo_prover::core::fields::m31::M31;
 
     use crate::decode::{Instruction, OpcodeExtension};
     use crate::memory::*;
     use crate::opcodes::StateTransitions;
-    use crate::plain::input_from_plain_casm;
+    use crate::plain::{adapt_finished_runner, program_from_casm};
     use crate::relocator::relocator_tests::{create_test_relocator, get_test_relocatble_trace};
     use crate::vm_import::RelocatedTraceEntry;
-    use crate::{casm_state, relocated_trace_entry};
+    use crate::{casm_state, relocated_trace_entry, ProverInput};
+
+    /// Translates a plain casm into a ProverInput by running the program and extracting the memory
+    /// and the state transitions.
+    fn input_from_plain_casm(casm: Vec<cairo_lang_casm::instructions::Instruction>) -> ProverInput {
+        let (program, program_len) = program_from_casm(casm);
+
+        let mut runner =
+            CairoRunner::new(&program, LayoutName::all_cairo_stwo, None, true, true, true)
+                .expect("Runner creation failed");
+        runner.initialize(true).expect("Initialization failed");
+        runner
+            .run_until_pc(
+                (runner.program_base.unwrap() + program_len).unwrap(),
+                &mut BuiltinHintProcessor::new_empty(),
+            )
+            .expect("Run failed");
+        runner.relocate(true).unwrap();
+        adapt_finished_runner(runner).expect("Failed to adapt finished runner")
+    }
 
     #[test]
     fn test_jmp_rel() {
--- stwo_cairo_prover/crates/recursion_bridge/src/proof_generation/mod.rs
+++ stwo_cairo_prover/crates/recursion_bridge/src/proof_generation/mod.rs
@@ -5,13 +5,36 @@ mod tests {
     use cairo_air::verifier::verify_cairo;
     use cairo_air::PreProcessedTraceVariant;
     use cairo_lang_casm::casm;
-    use stwo_cairo_adapter::plain::input_from_plain_casm_with_step_limit;
+    use cairo_vm::hint_processor::builtin_hint_processor::builtin_hint_processor_definition::BuiltinHintProcessor;
+    use cairo_vm::types::layout_name::LayoutName;
+    use cairo_vm::vm::runners::cairo_runner::CairoRunner;
+    use stwo_cairo_adapter::plain::{adapt_finished_runner, program_from_casm};
+    use stwo_cairo_adapter::ProverInput;
     use stwo_cairo_prover::prover::prove_cairo;
     use stwo_prover::core::pcs::PcsConfig;
     use stwo_prover::core::vcs::blake2_merkle::Blake2sMerkleChannel;
 
     pub fn project_root() -> PathBuf {
         std::path::PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap())
+    }
+
+    // NOTE: the proof will include `step_limit -1` steps.
+    fn input_from_plain_casm_with_step_limit(
+        casm: Vec<cairo_lang_casm::instructions::Instruction>,
+        step_limit: usize,
+    ) -> ProverInput {
+        let (program, _) = program_from_casm(casm);
+
+        let mut runner =
+            CairoRunner::new(&program, LayoutName::all_cairo_stwo, None, true, true, true)
+                .expect("Runner creation failed");
+        runner.initialize(true).expect("Initialization failed");
+        runner
+            .run_for_steps(step_limit, &mut BuiltinHintProcessor::new_empty())
+            .expect("Run failed");
+        runner.relocate(true).unwrap();
+
+        adapt_finished_runner(runner).expect("Failed to adapt finished runner")
     }
 
     // TODO(Ohad): this is temporary, develop better automation.
