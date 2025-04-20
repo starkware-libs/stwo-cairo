c8a9bbd14a767bd1e804510a345194690eb27b5b
Stav Beno <stav@starkware.co>
2025-04-20 17:27:51 +0300

    add to running log adapter different stages running

--- stwo_cairo_prover/crates/adapter/src/memory.rs
+++ stwo_cairo_prover/crates/adapter/src/memory.rs
@@ -5,6 +5,7 @@ use bytemuck::{bytes_of_mut, Pod, Zeroable};
 use cairo_vm::stdlib::collections::HashMap;
 use serde::{Deserialize, Serialize};
 use stwo_cairo_common::memory::{N_BITS_PER_FELT, N_M31_IN_SMALL_FELT252};
+use tracing::{span, Level};
 
 /// Prime 2^251 + 17 * 2^192 + 1 in little endian.
 pub const P_MIN_1: [u32; 8] = [
@@ -135,6 +136,7 @@ impl MemoryBuilder {
         config: MemoryConfig,
         iter: I,
     ) -> MemoryBuilder {
+        let _span = span!(Level::INFO, "MemoryBuilder::from_iter").entered();
         let memory_entries = iter.into_iter();
         let mut builder = Self::new(config);
         for entry in memory_entries {
