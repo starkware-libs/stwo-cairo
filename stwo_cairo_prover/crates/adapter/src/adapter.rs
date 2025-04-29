d49a178df547297ac7602954befeb04a052a2081
Stav Beno <stav@starkware.co>
2025-04-29 16:44:13 +0300

    change prover_input_info_to_prover_input -> adapter, and prover_input_from_vm_output to read_and_adapt_prover_input_info_file

--- stwo_cairo_prover/crates/adapter/src/adapter.rs
+++ stwo_cairo_prover/crates/adapter/src/adapter.rs
@@ -22,12 +22,10 @@ pub fn adapt_finished_runner(runner: CairoRunner) -> Result<ProverInput, VmImpor
         .get_prover_input_info()
         .expect("Unable to get prover input info");
 
-    prover_input_info_to_prover_input(&mut prover_input_info)
+    adapter(&mut prover_input_info)
 }
 
-pub fn prover_input_info_to_prover_input(
-    prover_input_info: &mut ProverInputInfo,
-) -> Result<ProverInput, VmImportError> {
+pub fn adapter(prover_input_info: &mut ProverInputInfo) -> Result<ProverInput, VmImportError> {
     BuiltinSegments::pad_relocatble_builtin_segments(
         &mut prover_input_info.relocatable_memory,
         prover_input_info.builtins_segments.clone(),
@@ -60,12 +58,12 @@ pub fn prover_input_info_to_prover_input(
     })
 }
 
-pub fn prover_input_from_vm_output(
+pub fn read_and_adapt_prover_input_info_file(
     prover_input_info_path: &Path,
 ) -> Result<ProverInput, VmImportError> {
     let _span: span::EnteredSpan = span!(Level::INFO, "adapter").entered();
 
-    prover_input_info_to_prover_input(&mut read_prover_input_info_file(prover_input_info_path))
+    adapter(&mut read_prover_input_info_file(prover_input_info_path))
 }
 
 #[cfg(test)]
@@ -73,7 +71,7 @@ pub fn prover_input_from_vm_output(
 mod tests {
     use serde_json::to_value;
 
-    use crate::adapter::prover_input_from_vm_output;
+    use crate::adapter::read_and_adapt_prover_input_info_file;
     use crate::test_utils::{
         get_compiled_cairo_program, get_prover_input_info_path, get_prover_input_path,
         prover_input_from_compiled_cairo_program, read_json, write_json,
@@ -95,7 +93,7 @@ mod tests {
         }
 
         let mut prover_input_b =
-            prover_input_from_vm_output(&get_prover_input_info_path(test_name))
+            read_and_adapt_prover_input_info_file(&get_prover_input_info_path(test_name))
                 .expect("Failed to create prover input from vm output");
         prover_input_b.inst_cache.sort_by_key(|(addr, _)| *addr);
 
