6631e1ab16afd2f162cbc80eeb45cf8aea8e0d9b
alon-f <44745918+alon-f@users.noreply.github.com>
2025-05-05 18:44:49 +0300

    More relation uses code gen (#865)
    <!-- Reviewable:start -->
    This change is [<img src="https://reviewable.io/review_button.svg" height="34" align="absmiddle" alt="Reviewable"/>](https://reviewable.io/reviews/starkware-libs/stwo-cairo/865)
    <!-- Reviewable:end -->

--- stwo_cairo_prover/crates/cairo-air/src/components/blake_round.rs
+++ stwo_cairo_prover/crates/cairo-air/src/components/blake_round.rs
@@ -2,6 +2,32 @@ use crate::components::prelude::*;
 use crate::components::subroutines::read_blake_word::ReadBlakeWord;
 
 pub const N_TRACE_COLUMNS: usize = 212;
+pub const RELATION_USES_PER_ROW: [RelationUse; 6] = [
+    RelationUse {
+        relation_id: "BlakeG",
+        uses: 8,
+    },
+    RelationUse {
+        relation_id: "BlakeRound",
+        uses: 1,
+    },
+    RelationUse {
+        relation_id: "BlakeRoundSigma",
+        uses: 1,
+    },
+    RelationUse {
+        relation_id: "MemoryAddressToId",
+        uses: 16,
+    },
+    RelationUse {
+        relation_id: "MemoryIdToBig",
+        uses: 16,
+    },
+    RelationUse {
+        relation_id: "RangeCheck_7_2_5",
+        uses: 16,
+    },
+];
 
 pub struct Eval {
     pub claim: Claim,
