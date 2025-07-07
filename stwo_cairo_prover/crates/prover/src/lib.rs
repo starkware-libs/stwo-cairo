66f64931b626ebbf6ab53efc82dc33fd5de79283
ohad-starkware <ohad.a@starkware.co>
2025-07-07 15:34:51 +0300

    removed allow module inception

--- stwo_cairo_prover/crates/prover/src/lib.rs
+++ stwo_cairo_prover/crates/prover/src/lib.rs
@@ -1,6 +1,5 @@
 #![feature(portable_simd, iter_array_chunks, array_chunks, raw_slice_split)]
-// TODO(Ohad): remove.
-#![allow(clippy::too_many_arguments, clippy::module_inception)]
+#![allow(clippy::too_many_arguments)]
 
 pub mod debug_tools;
 pub mod prover;
