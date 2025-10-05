f9424a952a020894383342346fa847a7aaeb38b3
Ohad Nir <ohad.n@starkware.co>
2025-10-05 17:41:22 +0300

    update blake opcode and its subroutines

--- stwo_cairo_prover/crates/cairo-air/src/components/subroutines/decode_instruction_472fe.rs
+++ stwo_cairo_prover/crates/cairo-air/src/components/subroutines/decode_instruction_472fe.rs
@@ -1,10 +1,10 @@
-// AIR version bc48deaa
+// AIR version c574c96b
 use crate::components::prelude::*;
 
 #[derive(Copy, Clone, Serialize, Deserialize, CairoSerialize)]
-pub struct DecodeInstruction64420 {}
+pub struct DecodeInstruction472Fe {}
 
-impl DecodeInstruction64420 {
+impl DecodeInstruction472Fe {
     #[allow(unused_parens)]
     #[allow(clippy::double_parens)]
     #[allow(non_snake_case)]
@@ -12,19 +12,18 @@ impl DecodeInstruction64420 {
     #[allow(unused_variables)]
     #[allow(clippy::too_many_arguments)]
     pub fn evaluate<E: EvalAtRow>(
-        [decode_instruction_64420_input_pc]: [E::F; 1],
+        [decode_instruction_472fe_input_pc]: [E::F; 1],
         offset0_col0: E::F,
         offset1_col1: E::F,
         offset2_col2: E::F,
         dst_base_fp_col3: E::F,
         op0_base_fp_col4: E::F,
         op1_base_fp_col5: E::F,
-        op1_base_ap_col6: E::F,
-        ap_update_add_1_col7: E::F,
-        opcode_extension_col8: E::F,
+        ap_update_add_1_col6: E::F,
+        opcode_extension_col7: E::F,
         verify_instruction_lookup_elements: &relations::VerifyInstruction,
         eval: &mut E,
-    ) -> [E::F; 3] {
+    ) -> [E::F; 4] {
         let M31_1 = E::F::from(M31::from(1));
         let M31_128 = E::F::from(M31::from(128));
         let M31_16 = E::F::from(M31::from(16));
@@ -45,28 +44,24 @@ impl DecodeInstruction64420 {
         eval.add_constraint(
             (op1_base_fp_col5.clone() * (M31_1.clone() - op1_base_fp_col5.clone())),
         );
-        // Flag op1_base_ap is a bit.
-        eval.add_constraint(
-            (op1_base_ap_col6.clone() * (M31_1.clone() - op1_base_ap_col6.clone())),
-        );
         // Flag ap_update_add_1 is a bit.
         eval.add_constraint(
-            (ap_update_add_1_col7.clone() * (M31_1.clone() - ap_update_add_1_col7.clone())),
+            (ap_update_add_1_col6.clone() * (M31_1.clone() - ap_update_add_1_col6.clone())),
         );
         eval.add_to_relation(RelationEntry::new(
             verify_instruction_lookup_elements,
             E::EF::one(),
             &[
-                decode_instruction_64420_input_pc.clone(),
+                decode_instruction_472fe_input_pc.clone(),
                 offset0_col0.clone(),
                 offset1_col1.clone(),
                 offset2_col2.clone(),
                 ((((dst_base_fp_col3.clone() * M31_8.clone())
                     + (op0_base_fp_col4.clone() * M31_16.clone()))
                     + (op1_base_fp_col5.clone() * M31_64.clone()))
-                    + (op1_base_ap_col6.clone() * M31_128.clone())),
-                (ap_update_add_1_col7.clone() * M31_32.clone()),
-                opcode_extension_col8.clone(),
+                    + ((M31_1.clone() - op1_base_fp_col5.clone()) * M31_128.clone())),
+                (ap_update_add_1_col6.clone() * M31_32.clone()),
+                opcode_extension_col7.clone(),
             ],
         ));
 
@@ -74,6 +69,7 @@ impl DecodeInstruction64420 {
             (offset0_col0.clone() - M31_32768.clone()),
             (offset1_col1.clone() - M31_32768.clone()),
             (offset2_col2.clone() - M31_32768.clone()),
+            (M31_1.clone() - op1_base_fp_col5.clone()),
         ]
     }
 }
