9d6cb8c73a3e1c2ef5b2709bd702d7a86d36be60
anatg <anatg@starkware.co>
2025-10-22 12:35:45 +0300

    Fix CR comments

--- stwo_cairo_prover/crates/common/src/preprocessed_columns/pedersen.rs
+++ stwo_cairo_prover/crates/common/src/preprocessed_columns/pedersen.rs
@@ -32,9 +32,8 @@ pub const ROWS_PER_WINDOW: usize = 1 << BITS_PER_WINDOW;
 
 pub const P0_SECTION_START: usize = 0;
 pub const P1_SECTION_START: usize = P0_SECTION_START + NUM_WINDOWS * ROWS_PER_WINDOW;
-pub const P2_SECTION_START: usize = P1_SECTION_START + 16;
-pub const P3_SECTION_START: usize = P2_SECTION_START + NUM_WINDOWS * ROWS_PER_WINDOW;
-pub const PEDERSEN_TABLE_N_ROWS: usize = P3_SECTION_START + 16;
+pub const P2_SECTION_START: usize = P1_SECTION_START + NUM_WINDOWS * ROWS_PER_WINDOW;
+pub const PEDERSEN_TABLE_N_ROWS: usize = P2_SECTION_START + 16 * 16;
 
 #[derive(Debug)]
 pub struct PedersenPoints {
@@ -112,7 +111,6 @@ fn create_block(
     base_point: &ProjectivePoint,
     n_rows: usize,
 ) -> Vec<AffinePoint> {
-    // Initialize the accumulator to -SHIFT_POINT
     let mut p = start_point.clone();
 
     // Compute the points in projective representation
