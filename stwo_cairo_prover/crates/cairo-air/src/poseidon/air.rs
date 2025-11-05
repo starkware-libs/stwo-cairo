08ff6197ba63966db85896ada2147c59fa10fb3f
Stav Beno <stav@starkware.co>
2025-11-05 13:16:50 +0200

    add poseidon aggregator component

--- stwo_cairo_prover/crates/cairo-air/src/poseidon/air.rs
+++ stwo_cairo_prover/crates/cairo-air/src/poseidon/air.rs
@@ -7,7 +7,7 @@ use stwo_constraint_framework::TraceLocationAllocator;
 use crate::air::{accumulate_relation_uses, CairoInteractionElements, RelationUsesDict};
 use crate::components::prelude::*;
 use crate::components::{
-    cube_252, indented_component_display, poseidon_3_partial_rounds_chain,
+    cube_252, indented_component_display, poseidon_3_partial_rounds_chain, poseidon_aggregator,
     poseidon_full_round_chain, poseidon_round_keys, range_check_252_width_27,
 };
 
@@ -38,6 +38,7 @@ impl PoseidonContextClaim {
 
 #[derive(Serialize, Deserialize, CairoSerialize, CairoDeserialize)]
 pub struct Claim {
+    pub poseidon_aggregator: poseidon_aggregator::Claim,
     pub poseidon_3_partial_rounds_chain: poseidon_3_partial_rounds_chain::Claim,
     pub poseidon_full_round_chain: poseidon_full_round_chain::Claim,
     pub cube_252: cube_252::Claim,
@@ -46,6 +47,7 @@ pub struct Claim {
 }
 impl Claim {
     pub fn mix_into(&self, channel: &mut impl Channel) {
+        self.poseidon_aggregator.mix_into(channel);
         self.poseidon_3_partial_rounds_chain.mix_into(channel);
         self.poseidon_full_round_chain.mix_into(channel);
         self.cube_252.mix_into(channel);
@@ -55,6 +57,7 @@ impl Claim {
 
     pub fn log_sizes(&self) -> TreeVec<Vec<u32>> {
         let log_sizes = [
+            self.poseidon_aggregator.log_sizes(),
             self.poseidon_3_partial_rounds_chain.log_sizes(),
             self.poseidon_full_round_chain.log_sizes(),
             self.cube_252.log_sizes(),
@@ -68,6 +71,7 @@ impl Claim {
 
     pub fn accumulate_relation_uses(&self, relation_uses: &mut RelationUsesDict) {
         let Self {
+            poseidon_aggregator,
             poseidon_3_partial_rounds_chain,
             poseidon_full_round_chain,
             cube_252,
@@ -98,6 +102,12 @@ impl Claim {
             range_check_252_width_27::RELATION_USES_PER_ROW,
             range_check_252_width_27.log_size,
         );
+
+        accumulate_relation_uses(
+            relation_uses,
+            poseidon_aggregator::RELATION_USES_PER_ROW,
+            poseidon_aggregator.log_size,
+        );
     }
 }
 
@@ -122,6 +132,7 @@ impl PoseidonContextInteractionClaim {
 
 #[derive(Serialize, Deserialize, CairoSerialize, CairoDeserialize)]
 pub struct InteractionClaim {
+    pub poseidon_aggregator: poseidon_aggregator::InteractionClaim,
     pub poseidon_3_partial_rounds_chain: poseidon_3_partial_rounds_chain::InteractionClaim,
     pub poseidon_full_round_chain: poseidon_full_round_chain::InteractionClaim,
     pub cube_252: cube_252::InteractionClaim,
@@ -130,6 +141,7 @@ pub struct InteractionClaim {
 }
 impl InteractionClaim {
     pub fn mix_into(&self, channel: &mut impl Channel) {
+        self.poseidon_aggregator.mix_into(channel);
         self.poseidon_3_partial_rounds_chain.mix_into(channel);
         self.poseidon_full_round_chain.mix_into(channel);
         self.cube_252.mix_into(channel);
@@ -139,6 +151,7 @@ impl InteractionClaim {
 
     pub fn sum(&self) -> QM31 {
         let mut sum = QM31::zero();
+        sum += self.poseidon_aggregator.claimed_sum;
         sum += self.poseidon_3_partial_rounds_chain.claimed_sum;
         sum += self.poseidon_full_round_chain.claimed_sum;
         sum += self.cube_252.claimed_sum;
@@ -183,6 +196,7 @@ impl std::fmt::Display for PoseidonContextComponents {
 }
 
 pub struct Components {
+    pub poseidon_aggregator: poseidon_aggregator::Component,
     pub poseidon_3_partial_rounds_chain: poseidon_3_partial_rounds_chain::Component,
     pub poseidon_full_round_chain: poseidon_full_round_chain::Component,
     pub cube_252: cube_252::Component,
@@ -196,6 +210,36 @@ impl Components {
         interaction_elements: &CairoInteractionElements,
         interaction_claim: &InteractionClaim,
     ) -> Self {
+        let poseidon_aggregator_component = poseidon_aggregator::Component::new(
+            tree_span_provider,
+            poseidon_aggregator::Eval {
+                claim: claim.claim.as_ref().unwrap().poseidon_aggregator,
+                poseidon_aggregator_lookup_elements: interaction_elements
+                    .poseidon_aggregator
+                    .clone(),
+                memory_id_to_big_lookup_elements: interaction_elements.memory_id_to_value.clone(),
+                poseidon_full_round_chain_lookup_elements: interaction_elements
+                    .poseidon_full_round_chain
+                    .clone(),
+                range_check_252_width_27_lookup_elements: interaction_elements
+                    .range_check_252_width_27
+                    .clone(),
+                poseidon_3_partial_rounds_chain_lookup_elements: interaction_elements
+                    .poseidon_3_partial_rounds_chain
+                    .clone(),
+                cube_252_lookup_elements: interaction_elements.cube_252.clone(),
+                range_check_3_3_3_3_3_lookup_elements: interaction_elements
+                    .range_checks
+                    .rc_3_3_3_3_3
+                    .clone(),
+                range_check_4_4_4_4_lookup_elements: interaction_elements
+                    .range_checks
+                    .rc_4_4_4_4
+                    .clone(),
+                range_check_4_4_lookup_elements: interaction_elements.range_checks.rc_4_4.clone(),
+            },
+            interaction_claim.poseidon_aggregator.claimed_sum,
+        );
         let poseidon_3_partial_rounds_chain_component =
             poseidon_3_partial_rounds_chain::Component::new(
                 tree_span_provider,
@@ -334,6 +378,7 @@ impl Components {
             interaction_claim.range_check_252_width_27.claimed_sum,
         );
         Self {
+            poseidon_aggregator: poseidon_aggregator_component,
             poseidon_3_partial_rounds_chain: poseidon_3_partial_rounds_chain_component,
             poseidon_full_round_chain: poseidon_full_round_chain_component,
             cube_252: cube_252_component,
@@ -344,6 +389,7 @@ impl Components {
 
     pub fn provers(&self) -> Vec<&dyn ComponentProver<SimdBackend>> {
         vec![
+            &self.poseidon_aggregator,
             &self.poseidon_3_partial_rounds_chain,
             &self.poseidon_full_round_chain,
             &self.cube_252,
@@ -355,6 +401,11 @@ impl Components {
 
 impl std::fmt::Display for Components {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(
+            f,
+            "PoseidonAggregator: {}",
+            indented_component_display(&self.poseidon_aggregator)
+        )?;
         writeln!(
             f,
             "Poseidon3PartialRoundsChain: {}",
@@ -376,7 +427,6 @@ impl std::fmt::Display for Components {
             "RangeCheck252Width27: {}",
             indented_component_display(&self.range_check_252_width_27)
         )?;
-
         Ok(())
     }
 }
