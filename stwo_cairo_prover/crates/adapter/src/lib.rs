89330c913b1b2e4befe6f9d3a8a37bb16afd254e
Noam Parzanchevski <noam.p@starkware.co>
2025-11-06 10:58:29 +0200

    aligned adapter output with sharp ExecutionResourcesStwo format

--- stwo_cairo_prover/crates/adapter/src/lib.rs
+++ stwo_cairo_prover/crates/adapter/src/lib.rs
@@ -99,32 +99,37 @@ pub struct MemoryTablesSizes {
 #[derive(Debug, Serialize, Deserialize)]
 pub struct ExecutionResources {
     /// Map opcode to the number of invocations.
-    pub opcode_instance_counts: HashMap<String, usize>,
+    pub opcodes_instance_counter: HashMap<String, usize>,
     /// Map builtin to the number of invocations.
-    pub builtin_instance_counts: HashMap<BuiltinName, usize>,
+    pub builtin_instance_counter: HashMap<String, usize>,
     /// Sizes of memory tables.
     pub memory_tables_sizes: MemoryTablesSizes,
     /// Number of verify instructions, corresponds to the number of unique pc values.
-    pub verify_instructions_count: usize,
+    pub n_verify_instructions: usize,
 }
 
 impl ExecutionResources {
     /// Create execution resources from prover input.
     pub fn from_prover_input(input: &ProverInput) -> Self {
         ExecutionResources {
-            opcode_instance_counts: input
+            opcodes_instance_counter: input
                 .state_transitions
                 .casm_states_by_opcode
                 .counts()
                 .into_iter()
                 .collect(),
-            builtin_instance_counts: input.builtin_segments.get_counts(),
+            builtin_instance_counter: input
+                .builtin_segments
+                .get_counts()
+                .into_iter()
+                .map(|(builtin, count)| (builtin.to_str_with_suffix().to_string(), count))
+                .collect(),
             memory_tables_sizes: MemoryTablesSizes {
                 address_to_id: input.memory.address_to_id.len(),
                 id_to_big: input.memory.f252_values.len(),
                 id_to_small: input.memory.small_values.len(),
             },
-            verify_instructions_count: input.pc_count,
+            n_verify_instructions: input.pc_count,
         }
     }
 }
