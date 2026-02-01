102556e9bc7cdbf4bf85eaeec2211a33f56fde7e
Ohad Nir <ohad.n@starkware.co>
2026-02-01 17:03:28 +0200

    remove par from the outer iter of add_packed_inputs, parallelise opcodes_write_trace

--- stwo_cairo_prover/crates/prover/src/witness/components/range_check_3_6_6_3.rs
+++ stwo_cairo_prover/crates/prover/src/witness/components/range_check_3_6_6_3.rs
@@ -65,7 +65,7 @@ impl ClaimGenerator {
     }
 
     pub fn add_packed_inputs(&self, packed_inputs: &[PackedInputType], relation_index: usize) {
-        packed_inputs.into_par_iter().for_each(|packed_input| {
+        packed_inputs.iter().for_each(|packed_input| {
             packed_input.unpack().iter().for_each(|input| {
                 self.add_input(input, relation_index);
             });
