b2330d14d2e59dac56f57021bf1b263adee8c997
Ilya Lesokhin <ilya@starkware.co>
2026-02-19 09:09:53 +0200

    Bump stwo.

--- stwo_cairo_prover/crates/cairo-serialize/src/serialize.rs
+++ stwo_cairo_prover/crates/cairo-serialize/src/serialize.rs
@@ -108,6 +108,7 @@ impl CairoSerialize for FriConfig {
             log_blowup_factor,
             log_last_layer_degree_bound,
             n_queries,
+            line_fold_step: _,
         } = self;
         log_blowup_factor.serialize(output);
         log_last_layer_degree_bound.serialize(output);
