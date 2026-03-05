c605879113d069e9de68a868578ae7ea45241e87
Ilya Lesokhin <ilya@starkware.co>
2026-03-05 08:19:57 +0200

    Support precomputed preprocessed_trace_polys.

--- stwo_cairo_prover/crates/prover/src/prover.rs
+++ stwo_cairo_prover/crates/prover/src/prover.rs
@@ -19,10 +19,11 @@ use stwo::core::poly::circle::CanonicCoset;
 use stwo::core::proof_of_work::GrindOps;
 use stwo::core::vcs_lifted::blake2_merkle::{Blake2sM31MerkleChannel, Blake2sMerkleChannel};
 use stwo::core::vcs_lifted::merkle_hasher::MerkleHasherLifted;
+use stwo::core::ColumnVec;
 use stwo::prover::backend::simd::SimdBackend;
 use stwo::prover::backend::BackendForChannel;
 use stwo::prover::mempool::BaseColumnPool;
-use stwo::prover::poly::circle::PolyOps;
+use stwo::prover::poly::circle::{CircleCoefficients, PolyOps};
 use stwo::prover::poly::twiddles::TwiddleTree;
 use stwo::prover::{prove_ex, CommitmentSchemeProver, ProvingError};
 use stwo_cairo_adapter::ProverInput;
@@ -90,14 +91,28 @@ where
             .half_coset,
     );
 
-    let base_column_pool = BaseColumnPool::new();
+    let preprocessed_trace_polys = SimdBackend::interpolate_columns(
+        gen_trace(Arc::new(
+            prover_params.preprocessed_trace.to_preprocessed_trace(),
+        )),
+        &twiddles,
+    );
+    // TODO(ilya): compute the commitment tree for the preprocessed trace.
 
-    prove_cairo_with_precompute::<MC>(&base_column_pool, &twiddles, input, prover_params)
+    let base_column_pool = BaseColumnPool::new();
+    prove_cairo_with_precompute::<MC>(
+        &base_column_pool,
+        &twiddles,
+        preprocessed_trace_polys,
+        input,
+        prover_params,
+    )
 }
 
 pub fn prove_cairo_with_precompute<MC: MerkleChannel>(
     base_column_pool: &BaseColumnPool<SimdBackend>,
     twiddles: &TwiddleTree<SimdBackend>,
+    preprocessed_trace_polys: ColumnVec<CircleCoefficients<SimdBackend>>,
     input: ProverInput,
     prover_params: ProverParameters,
 ) -> Result<CairoProof<MC::H>, ProvingError>
@@ -129,12 +144,12 @@ where
         commitment_scheme.set_store_polynomials_coefficients();
     }
     // Preprocessed trace.
-    let preprocessed_trace = Arc::new(preprocessed_trace.to_preprocessed_trace());
     let mut tree_builder = commitment_scheme.tree_builder();
-    tree_builder.extend_evals(gen_trace(preprocessed_trace.clone()));
+    tree_builder.extend_polys(preprocessed_trace_polys);
     tree_builder.commit(channel);
 
     // Run Cairo.
+    let preprocessed_trace = Arc::new(preprocessed_trace.clone().to_preprocessed_trace());
     let cairo_claim_generator = create_cairo_claim_generator(input, preprocessed_trace.clone());
     // Base trace.
     let mut tree_builder = commitment_scheme.tree_builder();
